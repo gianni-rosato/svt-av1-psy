fa85bf952f6c17bc623d44960c8157bd64a70493
hguermaz <hassen.guermazi@intel.com>
2019-05-28 10:00:31 -0700

    improved get_end_tx_depth

--- Source/Lib/Common/Codec/EbProductCodingLoop.c
+++ Source/Lib/Common/Codec/EbProductCodingLoop.c
@@ -2968,24 +2968,12 @@ static void tx_search_update_recon_sample_neighbor_array(
     return;
 }
 
-uint8_t get_end_tx_depth(uint8_t atb_mode, EB_SLICE slice_type, BlockSize bsize, uint8_t btype) {
+uint8_t get_end_tx_depth(ModeDecisionContext *context_ptr, uint8_t atb_mode, EB_SLICE slice_type, ModeDecisionCandidate *candidate_ptr, BlockSize bsize, uint8_t btype) {
    
     uint8_t tx_depth = 0;
-    if (atb_mode == 1) {
-        if (bsize == BLOCK_16X16||
-            bsize == BLOCK_16X8 ||
-            bsize == BLOCK_8X16 ||
-            bsize == BLOCK_8X8  ||
-            bsize == BLOCK_4X16 ||
-            bsize == BLOCK_16X4 ||
-            bsize == BLOCK_32X8 ||
-            bsize == BLOCK_8X32 ||
-            bsize == BLOCK_16X4 ||
-            bsize == BLOCK_4X16) {
-            tx_depth = 1;
-        }
-    }
-    else if (atb_mode == 2) {
+
+    // Hsan: shut transform partitioning if low energy residual; energy = SAD and threshold = f(width, height), and do not perform the check if ATB mode is full
+    if (atb_mode == 2 || ((context_ptr->decoupled_fast_loop_search_method == FULL_SAD_SEARCH) && (candidate_ptr->luma_fast_distortion > ((context_ptr->blk_geom->bwidth * context_ptr->blk_geom->bheight) << 2)))) {
         if (bsize == BLOCK_64X64 ||
             bsize == BLOCK_32X32 ||
             bsize == BLOCK_16X16 ||
@@ -4439,7 +4427,7 @@ void AV1PerformFullLoop(
         }
 #endif
 #if ATB_MD
-        uint8_t end_tx_depth = get_end_tx_depth(picture_control_set_ptr->parent_pcs_ptr->atb_mode, picture_control_set_ptr->slice_type, context_ptr->blk_geom->bsize, candidateBuffer->candidate_ptr->type);
+        uint8_t end_tx_depth = get_end_tx_depth(context_ptr, picture_control_set_ptr->parent_pcs_ptr->atb_mode, picture_control_set_ptr->slice_type, candidate_ptr, context_ptr->blk_geom->bsize, candidateBuffer->candidate_ptr->type);
         // Transform partitioning path (INTRA Luma)
         if (picture_control_set_ptr->parent_pcs_ptr->atb_mode && end_tx_depth && candidateBuffer->candidate_ptr->type == INTRA_MODE && candidateBuffer->candidate_ptr->use_intrabc == 0) {
             perform_intra_tx_partitioning(
