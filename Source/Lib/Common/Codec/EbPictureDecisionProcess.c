d764d15ef380059851d9b604bc193452c6f0fa10
Omran <omran.abdelkafi@intel.com>
2019-06-03 15:46:31 -0700

    Adopt MR_Mode's Intra_pred in encoder mode 0

--- Source/Lib/Common/Codec/EbPictureDecisionProcess.c
+++ Source/Lib/Common/Codec/EbPictureDecisionProcess.c
@@ -1443,7 +1443,9 @@ EbErrorType signal_derivation_multi_processes_oq(
 #if NEW_PRESETS
 #if SCREEN_CONTENT_SETTINGS
     if (sc_content_detected)
-        if (picture_control_set_ptr->enc_mode <= ENC_M2)
+        if (picture_control_set_ptr->enc_mode == ENC_M0) 
+			picture_control_set_ptr->intra_pred_mode = 0;
+        else if (picture_control_set_ptr->enc_mode <= ENC_M2)
             if (picture_control_set_ptr->temporal_layer_index == 0)
                 picture_control_set_ptr->intra_pred_mode = 1;
             else
@@ -1458,7 +1460,9 @@ EbErrorType signal_derivation_multi_processes_oq(
     else
 
 #endif
-        if (picture_control_set_ptr->enc_mode  <= ENC_M1)
+        if (picture_control_set_ptr->enc_mode == ENC_M0)
+			picture_control_set_ptr->intra_pred_mode = 0;
+        else if (picture_control_set_ptr->enc_mode  <= ENC_M1)
             if (picture_control_set_ptr->temporal_layer_index == 0)
                 picture_control_set_ptr->intra_pred_mode = 1;
             else
