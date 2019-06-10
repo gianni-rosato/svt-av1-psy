c34fa9d8c158c0410df77c86821ad8995f65c9b9
Cidana Developers <wenyao.liu@cidana.com>
2019-06-11 02:32:30 +0800

    Add intra pred unit test (#331)
    * Add intra pred unit test
    
    1. add unit test for all the intra prediction modes except palatte
       and intrabc.
    2. add unit test for upsampling and edge filter process.
    3. cfl_predict_lbd_avx2 and cfl_predict_hbd_avx2 will produce
        mismatch result compared with c version.
    
    * Rename Pixel to Sample in unit tests
    
    Use Sample to represent a value of a single component.

---
+++ test/intrapred_cfl_test.cc
@@ -0,0 +1,169 @@
+/*
+ * Copyright(c) 2019 Netflix, Inc.
+ * SPDX - License - Identifier: BSD - 2 - Clause - Patent
+ */
+
+/******************************************************************************
+ * @file intrapred_edge_filter_test.cc
+ *
+ * @brief Unit test for chroma from luma prediction:
+ * - cfl_predict_hbd_avx2
+ * - cfl_predict_lbd_avx2
+ *
+ * @author Cidana-Wenyao
+ *
+ ******************************************************************************/
+
+#include "gtest/gtest.h"
+#include "aom_dsp_rtcd.h"
+#include "EbDefinitions.h"
+#include "random.h"
+namespace {
+using svt_av1_test_tool::SVTRandom;
+
+using CFL_PRED_HBD = void (*)(const int16_t *pred_buf_q3, uint16_t *pred,
+                              int32_t pred_stride, uint16_t *dst,
+                              int32_t dst_stride, int32_t alpha_q3,
+                              int32_t bit_depth, int32_t width, int32_t height);
+using CFL_PRED_LBD = void (*)(const int16_t *pred_buf_q3, uint8_t *pred,
+                              int32_t pred_stride, uint8_t *dst,
+                              int32_t dst_stride, int32_t alpha_q3,
+                              int32_t bit_depth, int32_t width, int32_t height);
+/**
+ * @brief Unit test for chroma from luma prediction:
+ * - cfl_predict_hbd_avx2
+ * - cfl_predict_lbd_avx2
+ *
+ * Test strategy:
+ * Verify this assembly code by comparing with reference c implementation.
+ * Feed the same data and check test output and reference output.
+ * Define a templete class to handle the common process, and
+ * declare sub class to handle different bitdepth and function types.
+ *
+ * Expect result:
+ * Output from assemble functions should be the same with output from c.
+ *
+ * Test coverage:
+ * Test cases:
+ * pred buffer and dst buffer: Fill with random values
+ * TxSize: all the TxSize.
+ * alpha_q3: [-16, 16]
+ * BitDepth: 8bit and 10bit
+ */
+template <typename Sample, typename FuncType>
+class CflPredTest {
+  public:
+    CflPredTest() {
+        bd_ = 8;
+        ref_func_ = nullptr;
+        tst_func_ = nullptr;
+        common_init();
+    }
+
+    virtual ~CflPredTest() {
+    }
+
+    void RunAllTest() {
+        // for pred_buf, after sampling and subtracted from average
+        SVTRandom pred_rnd(bd_ + 3 + 1, true);
+        SVTRandom dst_rnd(4, false);
+        for (int tx = TX_4X4; tx < TX_SIZES_ALL; ++tx) {
+            const int c_w = tx_size_wide[tx] >> 1;
+            const int c_h = tx_size_high[tx] >> 1;
+            const int c_stride = CFL_BUF_LINE;
+            memset(pred_buf_q3, 0, sizeof(pred_buf_q3));
+            memset(dst_buf_ref_data_, 0, sizeof(dst_buf_ref_data_));
+            memset(dst_buf_tst_data_, 0, sizeof(dst_buf_tst_data_));
+
+            for (int alpha_q3 = -16; alpha_q3 <= 16; ++alpha_q3) {
+                // prepare data
+                for (int y = 0; y < c_h; ++y) {
+                    for (int x = 0; x < c_w; ++x) {
+                        pred_buf_q3[y * c_stride + x] = pred_rnd.random();
+                        dst_buf_ref_[y * c_stride + x] =
+                            dst_buf_tst_[y * c_stride + x] = dst_rnd.random();
+                    }
+                }
+
+                ref_func_(pred_buf_q3,
+                          dst_buf_ref_,
+                          CFL_BUF_LINE,
+                          dst_buf_ref_,
+                          CFL_BUF_LINE,
+                          alpha_q3,
+                          bd_,
+                          c_w,
+                          c_h);
+                tst_func_(pred_buf_q3,
+                          dst_buf_tst_,
+                          c_stride,
+                          dst_buf_tst_,
+                          c_stride,
+                          alpha_q3,
+                          bd_,
+                          c_w,
+                          c_h);
+
+                for (int y = 0; y < c_h; ++y) {
+                    for (int x = 0; x < c_w; ++x) {
+                        ASSERT_EQ(dst_buf_ref_[y * c_stride + x],
+                                  dst_buf_tst_[y * c_stride + x])
+                            << "tx_size: " << tx << " alpha_q3 " << alpha_q3
+                            << " expect " << dst_buf_ref_[y * c_stride + x]
+                            << " got " << dst_buf_tst_[y * c_stride + x]
+                            << " at [ " << x << " x " << y << " ]";
+                    }
+                }
+            }
+        }
+    }
+
+  protected:
+    void common_init() {
+        dst_buf_ref_ = reinterpret_cast<Sample *>(
+            ((intptr_t)(dst_buf_ref_data_) + alignment - 1) & ~(alignment - 1));
+        dst_buf_tst_ = reinterpret_cast<Sample *>(
+            ((intptr_t)(dst_buf_tst_data_) + alignment - 1) & ~(alignment - 1));
+    }
+
+    static const int alignment = 32;
+    int16_t pred_buf_q3[CFL_BUF_SQUARE];
+    Sample dst_buf_ref_data_[CFL_BUF_SQUARE + alignment - 1];
+    Sample dst_buf_tst_data_[CFL_BUF_SQUARE + alignment - 1];
+    Sample *dst_buf_ref_;
+    Sample *dst_buf_tst_;
+    FuncType ref_func_;
+    FuncType tst_func_;
+    int bd_;
+};
+
+class LbdCflPredTest : public CflPredTest<uint8_t, CFL_PRED_LBD> {
+  public:
+    LbdCflPredTest() {
+        bd_ = 8;
+        ref_func_ = cfl_predict_lbd_c;
+        tst_func_ = cfl_predict_lbd_avx2;
+        common_init();
+    }
+};
+
+class HbdCflPredTest : public CflPredTest<uint16_t, CFL_PRED_HBD> {
+  public:
+    HbdCflPredTest() {
+        bd_ = 10;
+        ref_func_ = cfl_predict_hbd_c;
+        tst_func_ = cfl_predict_hbd_avx2;
+        common_init();
+    }
+};
+
+#define TEST_CLASS(tc_name, type_name)     \
+    TEST(tc_name, match_test) {            \
+        type_name *test = new type_name(); \
+        test->RunAllTest();                \
+        delete test;                       \
+    }
+
+TEST_CLASS(LbdCflPredMatchTest, LbdCflPredTest)
+TEST_CLASS(HbdCflPredMatchTest, HbdCflPredTest)
+}  // namespace
---
+++ test/intrapred_test.cc
@@ -0,0 +1,380 @@
+/*
+ * Copyright(c) 2019 Netflix, Inc.
+ * SPDX - License - Identifier: BSD - 2 - Clause - Patent
+ */
+
+/******************************************************************************
+ * @file intrapred_test.cc
+ *
+ * @brief Unit test for intra {h, v}_pred, dc_pred, smooth_{h, v}_pred :
+ * - av1_highbd_{dc, h, v, smooth_h, smooth_v}_predictor_wxh_{sse2, avx2, ssse3}
+ * - av1_{dc, h, v, smooth_h, smooth_v}_predictor_wxh_{sse2, avx2, ssse3}
+ *
+ * @author Cidana-Wenyao
+ *
+ ******************************************************************************/
+
+#include "gtest/gtest.h"
+#include "aom_dsp_rtcd.h"
+#include "EbDefinitions.h"
+#include "random.h"
+
+namespace {
+using std::get;
+using std::make_tuple;
+using std::tuple;
+using svt_av1_test_tool::SVTRandom;
+
+const int count_test_block = 1000;
+
+using INTRAPRED_HBD = void (*)(uint16_t *dst, ptrdiff_t stride,
+                               const uint16_t *above, const uint16_t *left,
+                               int bd);
+using INTRAPRED_LBD = void (*)(uint8_t *dst, ptrdiff_t stride,
+                               const uint8_t *above, const uint8_t *left);
+
+using LBD_PARAMS = tuple<INTRAPRED_LBD, INTRAPRED_LBD, int, int, int>;
+using HBD_PARAMS = tuple<INTRAPRED_HBD, INTRAPRED_HBD, int, int, int>;
+
+/**
+ * @brief Unit test for intra prediction:
+ * - av1_highbd_{dc, h, v, smooth_h, smooth_v}_predictor_wxh_{sse2, avx2, ssse3}
+ * - av1_{dc, h, v, smooth_h, smooth_v}_predictor_wxh_{sse2, avx2, ssse3}
+ *
+ * Test strategy:
+ * Verify this assembly code by comparing with reference c implementation.
+ * Feed the same data and check test output and reference output.
+ * Define a templete class to handle the common process, and
+ * declare sub class to handle different bitdepth and function types.
+ *
+ * Expect result:
+ * Output from assemble functions should be the same with output from c.
+ *
+ * Test coverage:
+ * Test cases:
+ * Neighbor pixel buffer: Fill with random values
+ * TxSize: all the TxSize.
+ * BitDepth: 8bit and 10bit
+ *
+ */
+template <typename FuncType, typename Sample, typename TupleType>
+class AV1IntraPredTest : public ::testing::TestWithParam<TupleType> {
+  protected:
+    void prepare_data(SVTRandom &rnd, int cnt) {
+        if (cnt == 0) {
+            for (int x = -1; x <= bw_ * 2; x++)
+                above_row_[x] = (1 << bd_) - 1;
+
+            for (int y = 0; y < bh_; y++)
+                left_col_[y] = (1 << bd_) - 1;
+        } else {
+            for (int x = -1; x <= bw_ * 2; x++)
+                above_row_[x] = rnd.random();
+
+            for (int y = 0; y < bh_; y++)
+                left_col_[y] = rnd.random();
+        }
+        memset(dst_tst_data_, 0, sizeof(dst_tst_data_));
+        memset(dst_ref_data_, 0, sizeof(dst_ref_data_));
+    }
+
+  public:
+    void RunTest() {
+        SVTRandom rnd(0, (1 << bd_) - 1);
+        for (int i = 0; i < count_test_block; ++i) {
+            // prepare the neighbor pixels
+            prepare_data(rnd, i);
+
+            Predict();
+
+            for (int y = 0; y < bh_; y++) {
+                for (int x = 0; x < bw_; x++) {
+                    ASSERT_EQ(dst_ref_[x + y * stride_],
+                              dst_tst_[x + y * stride_])
+                        << " Failed on loop " << i << " location: x = " << x
+                        << " y = " << y;
+                }
+            }
+        }
+    }
+
+  protected:
+    void SetUp() override {
+        params_ = this->GetParam();
+        tst_func_ = get<0>(params_);
+        ref_func_ = get<1>(params_);
+        bw_ = get<2>(params_);
+        bh_ = get<3>(params_);
+        bd_ = get<4>(params_);
+        stride_ = bw_ * 3;
+        mask_ = (1 << bd_) - 1;
+        above_row_ = above_row_data_ + 16;
+        left_col_ = left_col_data_;
+        dst_tst_ = dst_tst_data_;
+        dst_ref_ = dst_ref_data_;
+    }
+
+    virtual void Predict() = 0;
+
+    Sample *above_row_;
+    Sample *left_col_;
+    Sample *dst_tst_;
+    Sample *dst_ref_;
+    DECLARE_ALIGNED(16, Sample, left_col_data_[2 * 64]);
+    DECLARE_ALIGNED(16, Sample, above_row_data_[2 * 64 + 64]);
+    DECLARE_ALIGNED(16, Sample, dst_tst_data_[3 * 64 * 64]);
+    DECLARE_ALIGNED(16, Sample, dst_ref_data_[3 * 64 * 64]);
+
+    ptrdiff_t stride_;
+    int bw_;  // block width
+    int bh_;  // block height
+    int mask_;
+    FuncType tst_func_;
+    FuncType ref_func_;
+    int bd_;
+
+    TupleType params_;
+};
+
+class HighbdIntraPredTest
+    : public AV1IntraPredTest<INTRAPRED_HBD, uint16_t, HBD_PARAMS> {
+  protected:
+    void Predict() {
+        const int bit_depth = bd_;
+        ref_func_(dst_ref_, stride_, above_row_, left_col_, bit_depth);
+        tst_func_(dst_tst_, stride_, above_row_, left_col_, bit_depth);
+    }
+};
+
+class LowbdIntraPredTest
+    : public AV1IntraPredTest<INTRAPRED_LBD, uint8_t, LBD_PARAMS> {
+  protected:
+    void Predict() {
+        ref_func_(dst_ref_, stride_, above_row_, left_col_);
+        tst_func_(dst_tst_, stride_, above_row_, left_col_);
+    }
+};
+
+TEST_P(HighbdIntraPredTest, match_test) {
+    RunTest();
+}
+
+TEST_P(LowbdIntraPredTest, match_test) {
+    RunTest();
+}
+
+// -----------------------------------------------------------------------------
+// High Bit Depth Tests
+#define hbd_entry(type, width, height, opt)                               \
+    make_tuple(&aom_highbd_##type##_predictor_##width##x##height##_##opt, \
+               &aom_highbd_##type##_predictor_##width##x##height##_c,     \
+               width,                                                     \
+               height,                                                    \
+               10)
+
+const HBD_PARAMS HighbdIntraPredTestVectorAsm[] = {
+    hbd_entry(dc_128, 16, 16, avx2),   hbd_entry(dc_128, 16, 32, avx2),
+    hbd_entry(dc_128, 16, 4, avx2),    hbd_entry(dc_128, 16, 64, avx2),
+    hbd_entry(dc_128, 16, 8, avx2),    hbd_entry(dc_128, 32, 16, avx2),
+    hbd_entry(dc_128, 32, 32, avx2),   hbd_entry(dc_128, 32, 64, avx2),
+    hbd_entry(dc_128, 32, 8, avx2),    hbd_entry(dc_128, 4, 16, sse2),
+    hbd_entry(dc_128, 4, 4, sse2),     hbd_entry(dc_128, 4, 8, sse2),
+    hbd_entry(dc_128, 64, 16, avx2),   hbd_entry(dc_128, 64, 32, avx2),
+    hbd_entry(dc_128, 64, 64, avx2),   hbd_entry(dc_128, 8, 16, sse2),
+    hbd_entry(dc_128, 8, 32, sse2),    hbd_entry(dc_128, 8, 4, sse2),
+    hbd_entry(dc_128, 8, 8, sse2),     hbd_entry(dc_left, 16, 16, avx2),
+    hbd_entry(dc_left, 16, 32, avx2),  hbd_entry(dc_left, 16, 4, avx2),
+    hbd_entry(dc_left, 16, 64, avx2),  hbd_entry(dc_left, 16, 8, avx2),
+    hbd_entry(dc_left, 32, 16, avx2),  hbd_entry(dc_left, 32, 32, avx2),
+    hbd_entry(dc_left, 32, 64, avx2),  hbd_entry(dc_left, 32, 8, avx2),
+    hbd_entry(dc_left, 4, 16, sse2),   hbd_entry(dc_left, 4, 4, sse2),
+    hbd_entry(dc_left, 4, 8, sse2),    hbd_entry(dc_left, 64, 16, avx2),
+    hbd_entry(dc_left, 64, 32, avx2),  hbd_entry(dc_left, 64, 64, avx2),
+    hbd_entry(dc_left, 8, 16, sse2),   hbd_entry(dc_left, 8, 32, sse2),
+    hbd_entry(dc_left, 8, 4, sse2),    hbd_entry(dc_left, 8, 8, sse2),
+    hbd_entry(dc, 16, 16, avx2),       hbd_entry(dc, 16, 32, avx2),
+    hbd_entry(dc, 16, 4, avx2),        hbd_entry(dc, 16, 64, avx2),
+    hbd_entry(dc, 16, 8, avx2),        hbd_entry(dc, 32, 16, avx2),
+    hbd_entry(dc, 32, 32, avx2),       hbd_entry(dc, 32, 64, avx2),
+    hbd_entry(dc, 32, 8, avx2),        hbd_entry(dc, 4, 16, sse2),
+    hbd_entry(dc, 4, 4, sse2),         hbd_entry(dc, 4, 8, sse2),
+    hbd_entry(dc, 64, 16, avx2),       hbd_entry(dc, 64, 32, avx2),
+    hbd_entry(dc, 64, 64, avx2),       hbd_entry(dc, 8, 16, sse2),
+    hbd_entry(dc, 8, 32, sse2),        hbd_entry(dc, 8, 4, sse2),
+    hbd_entry(dc, 8, 8, sse2),         hbd_entry(dc_top, 16, 16, avx2),
+    hbd_entry(dc_top, 16, 32, avx2),   hbd_entry(dc_top, 16, 4, avx2),
+    hbd_entry(dc_top, 16, 64, avx2),   hbd_entry(dc_top, 16, 8, avx2),
+    hbd_entry(dc_top, 32, 16, avx2),   hbd_entry(dc_top, 32, 32, avx2),
+    hbd_entry(dc_top, 32, 64, avx2),   hbd_entry(dc_top, 32, 8, avx2),
+    hbd_entry(dc_top, 4, 16, sse2),    hbd_entry(dc_top, 4, 4, sse2),
+    hbd_entry(dc_top, 4, 8, sse2),     hbd_entry(dc_top, 64, 16, avx2),
+    hbd_entry(dc_top, 64, 32, avx2),   hbd_entry(dc_top, 64, 64, avx2),
+    hbd_entry(dc_top, 8, 16, sse2),    hbd_entry(dc_top, 8, 4, sse2),
+    hbd_entry(dc_top, 8, 8, sse2),     hbd_entry(h, 16, 16, sse2),
+    hbd_entry(h, 16, 32, sse2),        hbd_entry(h, 16, 4, avx2),
+    hbd_entry(h, 16, 64, avx2),        hbd_entry(h, 16, 8, sse2),
+    hbd_entry(h, 32, 16, sse2),        hbd_entry(h, 32, 32, sse2),
+    hbd_entry(h, 32, 64, avx2),        hbd_entry(h, 32, 8, avx2),
+    hbd_entry(h, 4, 16, sse2),         hbd_entry(h, 4, 4, sse2),
+    hbd_entry(h, 4, 8, sse2),          hbd_entry(h, 64, 16, avx2),
+    hbd_entry(h, 64, 32, avx2),        hbd_entry(h, 64, 64, avx2),
+    hbd_entry(h, 8, 16, sse2),         hbd_entry(h, 8, 32, sse2),
+    hbd_entry(h, 8, 4, sse2),          hbd_entry(h, 8, 8, sse2),
+    hbd_entry(smooth_h, 16, 16, avx2), hbd_entry(smooth_h, 16, 32, avx2),
+    hbd_entry(smooth_h, 16, 4, avx2),  hbd_entry(smooth_h, 16, 64, avx2),
+    hbd_entry(smooth_h, 16, 8, avx2),  hbd_entry(smooth_h, 32, 16, avx2),
+    hbd_entry(smooth_h, 32, 32, avx2), hbd_entry(smooth_h, 32, 64, avx2),
+    hbd_entry(smooth_h, 32, 8, avx2),  hbd_entry(smooth_h, 4, 16, ssse3),
+    hbd_entry(smooth_h, 4, 4, ssse3),  hbd_entry(smooth_h, 4, 8, ssse3),
+    hbd_entry(smooth_h, 64, 16, avx2), hbd_entry(smooth_h, 64, 32, avx2),
+    hbd_entry(smooth_h, 64, 64, avx2), hbd_entry(smooth_h, 8, 16, avx2),
+    hbd_entry(smooth_h, 8, 32, avx2),  hbd_entry(smooth_h, 8, 4, avx2),
+    hbd_entry(smooth_h, 8, 8, avx2),   hbd_entry(smooth, 16, 16, avx2),
+    hbd_entry(smooth, 16, 32, avx2),   hbd_entry(smooth, 16, 4, avx2),
+    hbd_entry(smooth, 16, 64, avx2),   hbd_entry(smooth, 16, 8, avx2),
+    hbd_entry(smooth, 32, 16, avx2),   hbd_entry(smooth, 32, 32, avx2),
+    hbd_entry(smooth, 32, 64, avx2),   hbd_entry(smooth, 32, 8, avx2),
+    hbd_entry(smooth, 4, 16, ssse3),   hbd_entry(smooth, 4, 4, ssse3),
+    hbd_entry(smooth, 4, 8, ssse3),    hbd_entry(smooth, 64, 16, avx2),
+    hbd_entry(smooth, 64, 32, avx2),   hbd_entry(smooth, 64, 64, avx2),
+    hbd_entry(smooth, 8, 16, avx2),    hbd_entry(smooth, 8, 32, avx2),
+    hbd_entry(smooth, 8, 4, avx2),     hbd_entry(smooth, 8, 8, avx2),
+    hbd_entry(smooth_v, 16, 16, avx2), hbd_entry(smooth_v, 16, 32, avx2),
+    hbd_entry(smooth_v, 16, 4, avx2),  hbd_entry(smooth_v, 16, 64, avx2),
+    hbd_entry(smooth_v, 16, 8, avx2),  hbd_entry(smooth_v, 32, 16, avx2),
+    hbd_entry(smooth_v, 32, 32, avx2), hbd_entry(smooth_v, 32, 64, avx2),
+    hbd_entry(smooth_v, 32, 8, avx2),  hbd_entry(smooth_v, 4, 16, ssse3),
+    hbd_entry(smooth_v, 4, 4, ssse3),  hbd_entry(smooth_v, 4, 8, ssse3),
+    hbd_entry(smooth_v, 64, 16, avx2), hbd_entry(smooth_v, 64, 32, avx2),
+    hbd_entry(smooth_v, 64, 64, avx2), hbd_entry(smooth_v, 8, 16, avx2),
+    hbd_entry(smooth_v, 8, 32, avx2),  hbd_entry(smooth_v, 8, 4, avx2),
+    hbd_entry(smooth_v, 8, 8, avx2),   hbd_entry(v, 16, 16, avx2),
+    hbd_entry(v, 16, 32, avx2),        hbd_entry(v, 16, 4, avx2),
+    hbd_entry(v, 16, 64, avx2),        hbd_entry(v, 16, 8, avx2),
+    hbd_entry(v, 32, 16, avx2),        hbd_entry(v, 32, 32, avx2),
+    hbd_entry(v, 32, 64, avx2),        hbd_entry(v, 32, 8, avx2),
+    hbd_entry(v, 4, 16, sse2),         hbd_entry(v, 4, 4, sse2),
+    hbd_entry(v, 4, 8, sse2),          hbd_entry(v, 64, 16, avx2),
+    hbd_entry(v, 64, 32, avx2),        hbd_entry(v, 64, 64, avx2),
+    hbd_entry(v, 8, 16, sse2),         hbd_entry(v, 8, 32, sse2),
+    hbd_entry(v, 8, 4, sse2),          hbd_entry(v, 8, 8, sse2),
+};
+
+INSTANTIATE_TEST_CASE_P(intrapred, HighbdIntraPredTest,
+                        ::testing::ValuesIn(HighbdIntraPredTestVectorAsm));
+
+// ---------------------------------------------------------------------------
+// Low Bit Depth Tests
+#define lbd_entry(type, width, height, opt)                        \
+    LBD_PARAMS(&aom_##type##_predictor_##width##x##height##_##opt, \
+               &aom_##type##_predictor_##width##x##height##_c,     \
+               width,                                              \
+               height,                                             \
+               8)
+
+const LBD_PARAMS LowbdIntraPredTestVectorAsm[] = {
+    lbd_entry(dc, 4, 4, sse2),          lbd_entry(dc, 8, 8, sse2),
+    lbd_entry(dc, 16, 16, sse2),        lbd_entry(dc, 32, 32, avx2),
+    lbd_entry(dc, 64, 64, avx2),        lbd_entry(dc, 16, 32, sse2),
+    lbd_entry(dc, 16, 4, sse2),         lbd_entry(dc, 16, 64, sse2),
+    lbd_entry(dc, 16, 8, sse2),         lbd_entry(dc, 32, 16, avx2),
+    lbd_entry(dc, 32, 64, avx2),        lbd_entry(dc, 32, 8, sse2),
+    lbd_entry(dc, 4, 16, sse2),         lbd_entry(dc, 4, 8, sse2),
+    lbd_entry(dc, 64, 16, avx2),        lbd_entry(dc, 64, 32, avx2),
+    lbd_entry(dc, 8, 16, sse2),         lbd_entry(dc, 8, 32, sse2),
+    lbd_entry(dc, 8, 4, sse2),          lbd_entry(dc_left, 4, 4, sse2),
+    lbd_entry(dc_left, 8, 8, sse2),     lbd_entry(dc_left, 16, 16, sse2),
+    lbd_entry(dc_left, 32, 32, avx2),   lbd_entry(dc_left, 64, 64, avx2),
+    lbd_entry(dc_left, 16, 32, sse2),   lbd_entry(dc_left, 16, 4, sse2),
+    lbd_entry(dc_left, 16, 64, sse2),   lbd_entry(dc_left, 16, 8, sse2),
+    lbd_entry(dc_left, 32, 16, avx2),   lbd_entry(dc_left, 32, 64, avx2),
+    lbd_entry(dc_left, 32, 8, sse2),    lbd_entry(dc_left, 4, 16, sse2),
+    lbd_entry(dc_left, 4, 8, sse2),     lbd_entry(dc_left, 64, 16, avx2),
+    lbd_entry(dc_left, 64, 32, avx2),   lbd_entry(dc_left, 8, 16, sse2),
+    lbd_entry(dc_left, 8, 32, sse2),    lbd_entry(dc_left, 8, 4, sse2),
+    lbd_entry(dc_top, 4, 4, sse2),      lbd_entry(dc_top, 8, 8, sse2),
+    lbd_entry(dc_top, 16, 16, sse2),    lbd_entry(dc_top, 32, 32, avx2),
+    lbd_entry(dc_top, 64, 64, avx2),    lbd_entry(dc_top, 16, 32, sse2),
+    lbd_entry(dc_top, 16, 4, sse2),     lbd_entry(dc_top, 16, 64, sse2),
+    lbd_entry(dc_top, 16, 8, sse2),     lbd_entry(dc_top, 32, 16, avx2),
+    lbd_entry(dc_top, 32, 64, avx2),    lbd_entry(dc_top, 32, 8, sse2),
+    lbd_entry(dc_top, 4, 16, sse2),     lbd_entry(dc_top, 4, 8, sse2),
+    lbd_entry(dc_top, 64, 16, avx2),    lbd_entry(dc_top, 64, 32, avx2),
+    lbd_entry(dc_top, 8, 16, sse2),     lbd_entry(dc_top, 8, 32, sse2),
+    lbd_entry(dc_top, 8, 4, sse2),      lbd_entry(dc_128, 4, 4, sse2),
+    lbd_entry(dc_128, 8, 8, sse2),      lbd_entry(dc_128, 16, 16, sse2),
+    lbd_entry(dc_128, 32, 32, avx2),    lbd_entry(dc_128, 64, 64, avx2),
+    lbd_entry(dc_128, 16, 32, sse2),    lbd_entry(dc_128, 16, 4, sse2),
+    lbd_entry(dc_128, 16, 64, sse2),    lbd_entry(dc_128, 16, 8, sse2),
+    lbd_entry(dc_128, 32, 16, avx2),    lbd_entry(dc_128, 32, 64, avx2),
+    lbd_entry(dc_128, 32, 8, sse2),     lbd_entry(dc_128, 4, 16, sse2),
+    lbd_entry(dc_128, 4, 8, sse2),      lbd_entry(dc_128, 64, 16, avx2),
+    lbd_entry(dc_128, 64, 32, avx2),    lbd_entry(dc_128, 8, 16, sse2),
+    lbd_entry(dc_128, 8, 32, sse2),     lbd_entry(dc_128, 8, 4, sse2),
+    lbd_entry(smooth_h, 64, 64, ssse3), lbd_entry(smooth_h, 32, 32, ssse3),
+    lbd_entry(smooth_h, 16, 16, ssse3), lbd_entry(smooth_h, 8, 8, ssse3),
+    lbd_entry(smooth_h, 4, 4, ssse3),   lbd_entry(smooth_h, 16, 32, ssse3),
+    lbd_entry(smooth_h, 16, 4, ssse3),  lbd_entry(smooth_h, 16, 64, ssse3),
+    lbd_entry(smooth_h, 16, 8, ssse3),  lbd_entry(smooth_h, 32, 16, ssse3),
+    lbd_entry(smooth_h, 32, 64, ssse3), lbd_entry(smooth_h, 32, 8, ssse3),
+    lbd_entry(smooth_h, 4, 16, ssse3),  lbd_entry(smooth_h, 4, 8, ssse3),
+    lbd_entry(smooth_h, 64, 16, ssse3), lbd_entry(smooth_h, 64, 32, ssse3),
+    lbd_entry(smooth_h, 8, 16, ssse3),  lbd_entry(smooth_h, 8, 32, ssse3),
+    lbd_entry(smooth_h, 8, 4, ssse3),   lbd_entry(smooth_v, 64, 64, ssse3),
+    lbd_entry(smooth_v, 32, 32, ssse3), lbd_entry(smooth_v, 16, 16, ssse3),
+    lbd_entry(smooth_v, 8, 8, ssse3),   lbd_entry(smooth_v, 4, 4, ssse3),
+    lbd_entry(smooth_v, 16, 32, ssse3), lbd_entry(smooth_v, 16, 4, ssse3),
+    lbd_entry(smooth_v, 16, 64, ssse3), lbd_entry(smooth_v, 16, 8, ssse3),
+    lbd_entry(smooth_v, 32, 16, ssse3), lbd_entry(smooth_v, 32, 64, ssse3),
+    lbd_entry(smooth_v, 32, 8, ssse3),  lbd_entry(smooth_v, 4, 16, ssse3),
+    lbd_entry(smooth_v, 4, 8, ssse3),   lbd_entry(smooth_v, 64, 16, ssse3),
+    lbd_entry(smooth_v, 64, 32, ssse3), lbd_entry(smooth_v, 8, 16, ssse3),
+    lbd_entry(smooth_v, 8, 32, ssse3),  lbd_entry(smooth_v, 8, 4, ssse3),
+    lbd_entry(smooth, 64, 64, ssse3),   lbd_entry(smooth, 32, 32, ssse3),
+    lbd_entry(smooth, 16, 16, ssse3),   lbd_entry(smooth, 8, 8, ssse3),
+    lbd_entry(smooth, 4, 4, ssse3),     lbd_entry(smooth, 16, 32, ssse3),
+    lbd_entry(smooth, 16, 4, ssse3),    lbd_entry(smooth, 16, 64, ssse3),
+    lbd_entry(smooth, 16, 8, ssse3),    lbd_entry(smooth, 32, 16, ssse3),
+    lbd_entry(smooth, 32, 64, ssse3),   lbd_entry(smooth, 32, 8, ssse3),
+    lbd_entry(smooth, 4, 16, ssse3),    lbd_entry(smooth, 4, 8, ssse3),
+    lbd_entry(smooth, 64, 16, ssse3),   lbd_entry(smooth, 64, 32, ssse3),
+    lbd_entry(smooth, 8, 16, ssse3),    lbd_entry(smooth, 8, 32, ssse3),
+    lbd_entry(smooth, 8, 4, ssse3),     lbd_entry(v, 4, 4, sse2),
+    lbd_entry(v, 8, 8, sse2),           lbd_entry(v, 16, 16, sse2),
+    lbd_entry(v, 32, 32, avx2),         lbd_entry(v, 64, 64, avx2),
+    lbd_entry(v, 16, 32, sse2),         lbd_entry(v, 16, 4, sse2),
+    lbd_entry(v, 16, 64, sse2),         lbd_entry(v, 16, 8, sse2),
+    lbd_entry(v, 32, 16, avx2),         lbd_entry(v, 32, 64, avx2),
+    lbd_entry(v, 32, 8, sse2),          lbd_entry(v, 4, 16, sse2),
+    lbd_entry(v, 4, 8, sse2),           lbd_entry(v, 64, 16, avx2),
+    lbd_entry(v, 64, 32, avx2),         lbd_entry(v, 8, 16, sse2),
+    lbd_entry(v, 8, 32, sse2),          lbd_entry(v, 8, 4, sse2),
+    lbd_entry(h, 4, 4, sse2),           lbd_entry(h, 8, 8, sse2),
+    lbd_entry(h, 16, 16, sse2),         lbd_entry(h, 32, 32, avx2),
+    lbd_entry(h, 64, 64, sse2),         lbd_entry(h, 16, 32, sse2),
+    lbd_entry(h, 16, 4, sse2),          lbd_entry(h, 16, 64, sse2),
+    lbd_entry(h, 16, 8, sse2),          lbd_entry(h, 32, 16, sse2),
+    lbd_entry(h, 32, 64, sse2),         lbd_entry(h, 32, 8, sse2),
+    lbd_entry(h, 4, 16, sse2),          lbd_entry(h, 4, 8, sse2),
+    lbd_entry(h, 64, 16, sse2),         lbd_entry(h, 64, 32, sse2),
+    lbd_entry(h, 8, 16, sse2),          lbd_entry(h, 8, 32, sse2),
+    lbd_entry(h, 8, 4, sse2),           lbd_entry(paeth, 16, 16, ssse3),
+    lbd_entry(paeth, 16, 16, avx2),     lbd_entry(paeth, 16, 32, ssse3),
+    lbd_entry(paeth, 16, 32, avx2),     lbd_entry(paeth, 16, 4, ssse3),
+    lbd_entry(paeth, 16, 64, ssse3),    lbd_entry(paeth, 16, 64, avx2),
+    lbd_entry(paeth, 16, 8, ssse3),     lbd_entry(paeth, 16, 8, avx2),
+    lbd_entry(paeth, 32, 16, ssse3),    lbd_entry(paeth, 32, 16, avx2),
+    lbd_entry(paeth, 32, 32, ssse3),    lbd_entry(paeth, 32, 32, avx2),
+    lbd_entry(paeth, 32, 64, ssse3),    lbd_entry(paeth, 32, 64, avx2),
+    lbd_entry(paeth, 32, 8, ssse3),     lbd_entry(paeth, 4, 16, ssse3),
+    lbd_entry(paeth, 4, 4, ssse3),      lbd_entry(paeth, 4, 8, ssse3),
+    lbd_entry(paeth, 64, 16, ssse3),    lbd_entry(paeth, 64, 16, avx2),
+    lbd_entry(paeth, 64, 32, ssse3),    lbd_entry(paeth, 64, 32, avx2),
+    lbd_entry(paeth, 64, 64, ssse3),    lbd_entry(paeth, 64, 64, avx2),
+    lbd_entry(paeth, 8, 16, ssse3),     lbd_entry(paeth, 8, 32, ssse3),
+    lbd_entry(paeth, 8, 4, ssse3),      lbd_entry(paeth, 8, 8, ssse3),
+};
+
+INSTANTIATE_TEST_CASE_P(intrapred, LowbdIntraPredTest,
+                        ::testing::ValuesIn(LowbdIntraPredTestVectorAsm));
+}  // namespace
