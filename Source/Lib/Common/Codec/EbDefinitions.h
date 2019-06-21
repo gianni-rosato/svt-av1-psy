7e31fde2cbceed71389cbe3dd186e6c64cf76d35
Joel Sole <jsole@netflix.com>
2019-06-21 09:57:06 -0700

    Macro removal: M9_NFL 0

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -146,7 +146,7 @@ extern "C" {
 
 
 
-#define M9_NFL                            0 // VP9 4K NFL settings; NFL = 3
+
 #define M9_CDEF                           0 // CDEF off
 #define M9_TX_SEARCH                      0 // Tx search off
 #define VP9_ADP                           0 // VP9 4K ADP budget;  (121,110,100 but different injection) (budget = f (layer index))
