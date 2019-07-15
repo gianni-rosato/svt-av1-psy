b11f07ce66c2d69f3b283b286061557f4b1be505
L. Zhang <lznewjersey@gmail.com>
2019-07-15 00:40:08 -0700

    Align more memories for SIMD

--- Source/Lib/Common/Codec/EbMotionEstimationContext.h
+++ Source/Lib/Common/Codec/EbMotionEstimationContext.h
@@ -348,9 +348,9 @@ extern "C" {
         uint32_t                     *p_best_mv8x32;
         uint32_t                     *p_best_mv64x16;
         uint32_t                     *p_best_mv16x64;
-        uint32_t                      p_sad32x32[4];
-        uint32_t                      p_sad16x16[16];
-        uint32_t                      p_sad8x8[64];
+        EB_ALIGN(16) uint32_t         p_sad32x32[4];
+        EB_ALIGN(64) uint32_t         p_sad16x16[16];
+        EB_ALIGN(64) uint32_t         p_sad8x8[64];
 
         uint8_t                       psub_pel_direction64x64;
         uint8_t                       psub_pel_direction32x32[4];
@@ -408,9 +408,9 @@ extern "C" {
         uint8_t                     *p_best_nsq32x32;
         uint8_t                     *p_best_nsq64x64;
         uint16_t                     *p_eight_pos_sad16x16;
-        uint32_t                      p_eight_sad32x32[4][8];
-        uint32_t                      p_eight_sad16x16[16][8];
-        uint32_t                      p_eight_sad8x8[64][8];
+        EB_ALIGN(64) uint32_t         p_eight_sad32x32[4][8];
+        EB_ALIGN(64) uint32_t         p_eight_sad16x16[16][8];
+        EB_ALIGN(64) uint32_t         p_eight_sad8x8[64][8];
         EbBitFraction               *mvd_bits_array;
         uint64_t                      lambda;
         uint8_t                       hme_search_type;
