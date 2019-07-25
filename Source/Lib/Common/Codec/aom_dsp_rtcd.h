hguermaz/SVT-AV1

avx 512 intrinsics support for highbd_fwd_txfm2d_16x16 all 16 transforms
Following are the results of Unit tests and performance tests compared
against AVX2

av1_fwd_txfm_16x16 DCT_DCT     1.42x    6848.39         9744.99
        av1_fwd_txfm_16x16 ADST_DCT     1.34x    7617.16         10201.58
        av1_fwd_txfm_16x16 DCT_ADST     1.34x    7610.70         10164.90
       av1_fwd_txfm_16x16 ADST_ADST     1.31x    8604.63         11229.74
    av1_fwd_txfm_16x16 FLIPADST_DCT     1.52x    6945.68         10565.52
    av1_fwd_txfm_16x16 DCT_FLIPADST     1.30x    7902.89         10305.39
av1_fwd_txfm_16x16 FLIPADST_FLIPADST    1.41x    8199.65         11537.69
   av1_fwd_txfm_16x16 ADST_FLIPADST     1.48x    8143.18         12073.94
   av1_fwd_txfm_16x16 FLIPADST_ADST     1.40x    8568.55         11957.64
            av1_fwd_txfm_16x16 IDTX     2.09x    1675.73         3510.61
           av1_fwd_txfm_16x16 V_DCT     1.71x    3324.54         5678.31
           av1_fwd_txfm_16x16 H_DCT     1.54x    4874.80         7523.28
          av1_fwd_txfm_16x16 V_ADST     1.69x    4161.85         7032.22
          av1_fwd_txfm_16x16 H_ADST     1.52x    5744.67         8732.08
      av1_fwd_txfm_16x16 V_FLIPADST     1.69x    4222.15         7123.12
      av1_fwd_txfm_16x16 H_FLIPADST     1.50x    6104.69         9129.65

Source/Lib/Common/Codec/aom_dsp_rtcd.h
@@ -249,6 +249,7 @@ extern "C" {
 
     void Av1TransformTwoD_16x16_c(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
     void av1_fwd_txfm2d_16x16_avx2(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
+    void av1_fwd_txfm2d_16x16_avx512(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
     RTCD_EXTERN void(*av1_fwd_txfm2d_16x16)(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
 
     void Av1TransformTwoD_8x8_c(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
@@ -3205,7 +3206,11 @@ extern "C" {
         av1_fwd_txfm2d_32x32 = Av1TransformTwoD_32x32_c;
         if (flags & HAS_AVX2) av1_fwd_txfm2d_32x32 = av1_fwd_txfm2d_32x32_avx2;
         av1_fwd_txfm2d_16x16 = Av1TransformTwoD_16x16_c;
+#ifndef NON_AVX512_SUPPORT
+        if (flags & HAS_AVX2) av1_fwd_txfm2d_16x16 = av1_fwd_txfm2d_16x16_avx512;
+#else
         if (flags & HAS_AVX2) av1_fwd_txfm2d_16x16 = av1_fwd_txfm2d_16x16_avx2;
+#endif       
         av1_fwd_txfm2d_8x8 = Av1TransformTwoD_8x8_c;
         if (flags & HAS_AVX2) av1_fwd_txfm2d_8x8 = av1_fwd_txfm2d_8x8_avx2;
         av1_fwd_txfm2d_4x4 = Av1TransformTwoD_4x4_c;

gianni-rosato/svt-av1-psy

Source/Lib/Common/Codec/aom_dsp_rtcd.h
@@ -243,6 +243,7 @@ extern "C" {
 
     void Av1TransformTwoD_16x16_c(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
     void eb_av1_fwd_txfm2d_16x16_avx2(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
+    void av1_fwd_txfm2d_16x16_avx512(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
     RTCD_EXTERN void(*eb_av1_fwd_txfm2d_16x16)(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
 
     void Av1TransformTwoD_8x8_c(int16_t *input, int32_t *output, uint32_t input_stride, TxType transform_type, uint8_t  bit_depth);
@@ -3243,7 +3244,11 @@ extern "C" {
         eb_av1_fwd_txfm2d_32x32 = Av1TransformTwoD_32x32_c;
         if (flags & HAS_AVX2) eb_av1_fwd_txfm2d_32x32 = eb_av1_fwd_txfm2d_32x32_avx2;
         eb_av1_fwd_txfm2d_16x16 = Av1TransformTwoD_16x16_c;
+#ifndef NON_AVX512_SUPPORT
+        if (flags & HAS_AVX2) eb_av1_fwd_txfm2d_16x16 = av1_fwd_txfm2d_16x16_avx512;
+#else
         if (flags & HAS_AVX2) eb_av1_fwd_txfm2d_16x16 = eb_av1_fwd_txfm2d_16x16_avx2;
+#endif       
         eb_av1_fwd_txfm2d_8x8 = Av1TransformTwoD_8x8_c;
         if (flags & HAS_AVX2) eb_av1_fwd_txfm2d_8x8 = eb_av1_fwd_txfm2d_8x8_avx2;
         eb_av1_fwd_txfm2d_4x4 = Av1TransformTwoD_4x4_c;