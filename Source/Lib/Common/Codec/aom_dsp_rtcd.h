hguermaz/SVT-AV1

Add AVX 512 intrinsics for highbd_inv_16x16 all transform types
Following are the results of Unit tests and performance tests compared
against AVX2

 av1_inv_txfm_16x16 DCT_DCT     1.40x    6690.10         9398.26
        av1_inv_txfm_16x16 ADST_DCT     1.41x    7591.71         10691.76
        av1_inv_txfm_16x16 DCT_ADST     1.42x    7482.08         10608.75
       av1_inv_txfm_16x16 ADST_ADST     1.43x    8305.86         11885.31
    av1_inv_txfm_16x16 FLIPADST_DCT     1.45x    7529.86         10900.24
    av1_inv_txfm_16x16 DCT_FLIPADST     1.38x    8002.28         11015.85
av1_inv_txfm_16x16 FLIPADST_FLIPADST    1.45x    8745.55         12642.18
   av1_inv_txfm_16x16 ADST_FLIPADST     1.42x    8965.24         12716.10
   av1_inv_txfm_16x16 FLIPADST_ADST     1.41x    8447.21         11938.94
            av1_inv_txfm_16x16 IDTX     2.12x    1633.17         3459.71
           av1_inv_txfm_16x16 V_DCT     1.70x    3371.94         5746.88
           av1_inv_txfm_16x16 H_DCT     1.49x    5003.06         7478.93
          av1_inv_txfm_16x16 V_ADST     1.67x    4273.57         7121.52
          av1_inv_txfm_16x16 H_ADST     1.47x    5752.89         8468.71
      av1_inv_txfm_16x16 V_FLIPADST     1.69x    4148.47         7011.73
      av1_inv_txfm_16x16 H_FLIPADST     1.47x    6057.87         8876.66

Source/Lib/Common/Codec/aom_dsp_rtcd.h
@@ -357,6 +357,7 @@ extern "C" {
     void av1_inv_txfm2d_add_16x16_c(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     void av1_inv_txfm2d_add_16x16_sse4_1(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     void av1_inv_txfm2d_add_16x16_avx2(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
+    void av1_inv_txfm2d_add_16x16_avx512(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     RTCD_EXTERN void(*av1_inv_txfm2d_add_16x16)(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
 
     void av1_inv_txfm2d_add_32x32_c(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
@@ -2506,8 +2507,6 @@ extern "C" {
         aom_highbd_quantize_b = aom_highbd_quantize_b_c;
         if (flags & HAS_AVX2) aom_highbd_quantize_b = aom_highbd_quantize_b_avx2;
 
-        av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_c;
-        if (flags & HAS_AVX2) av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_avx2;
         av1_inv_txfm2d_add_32x32 = av1_inv_txfm2d_add_32x32_c;
         if (flags & HAS_AVX2) av1_inv_txfm2d_add_32x32 = av1_inv_txfm2d_add_32x32_avx2;
         av1_inv_txfm2d_add_4x4 = av1_inv_txfm2d_add_4x4_c;
@@ -2516,6 +2515,12 @@ extern "C" {
         if (flags & HAS_SSE4_1) av1_inv_txfm2d_add_64x64 = av1_inv_txfm2d_add_64x64_sse4_1;
         av1_inv_txfm2d_add_8x8 = av1_inv_txfm2d_add_8x8_c;
         if (flags & HAS_AVX2) av1_inv_txfm2d_add_8x8 = av1_inv_txfm2d_add_8x8_avx2;
+        av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_c;
+#ifndef NON_AVX512_SUPPORT
+        if (flags & HAS_AVX2) av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_avx512;
+#else
+        if (flags & HAS_AVX2) av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_avx2;
+#endif
 
         av1_inv_txfm2d_add_8x16 = av1_inv_txfm2d_add_8x16_c;
         if (flags & HAS_AVX2) av1_inv_txfm2d_add_8x16 = av1_highbd_inv_txfm_add_avx2;

gianni-rosato/svt-av1-psy

Source/Lib/Common/Codec/aom_dsp_rtcd.h
@@ -370,6 +370,7 @@ extern "C" {
     void eb_av1_inv_txfm2d_add_16x16_c(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     void eb_av1_inv_txfm2d_add_16x16_sse4_1(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     void eb_av1_inv_txfm2d_add_16x16_avx2(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
+    void av1_inv_txfm2d_add_16x16_avx512(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     RTCD_EXTERN void(*eb_av1_inv_txfm2d_add_16x16)(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
 
     void eb_av1_inv_txfm2d_add_32x32_c(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
@@ -2502,7 +2503,6 @@ extern "C" {
         if (flags & HAS_AVX2) eb_aom_highbd_quantize_b = eb_aom_highbd_quantize_b_avx2;
 
         eb_av1_inv_txfm2d_add_16x16 = eb_av1_inv_txfm2d_add_16x16_c;
-        if (flags & HAS_AVX2) eb_av1_inv_txfm2d_add_16x16 = eb_av1_inv_txfm2d_add_16x16_avx2;
         eb_av1_inv_txfm2d_add_32x32 = eb_av1_inv_txfm2d_add_32x32_c;
         if (flags & HAS_AVX2) eb_av1_inv_txfm2d_add_32x32 = eb_av1_inv_txfm2d_add_32x32_avx2;
         eb_av1_inv_txfm2d_add_4x4 = eb_av1_inv_txfm2d_add_4x4_c;
@@ -2541,6 +2541,11 @@ extern "C" {
         eb_av1_inv_txfm2d_add_16x4 = eb_av1_inv_txfm2d_add_16x4_c;
         if (flags & HAS_SSE4_1) eb_av1_inv_txfm2d_add_16x4 = eb_av1_inv_txfm2d_add_16x4_sse4_1;
 
+#ifndef NON_AVX512_SUPPORT
+        if (flags & HAS_AVX2) eb_av1_inv_txfm2d_add_16x16 = av1_inv_txfm2d_add_16x16_avx512;
+#else
+        if (flags & HAS_AVX2) eb_av1_inv_txfm2d_add_16x16 = eb_av1_inv_txfm2d_add_16x16_avx2;
+#endif
         eb_av1_inv_txfm_add = eb_av1_inv_txfm_add_c;
         if (flags & HAS_SSSE3) eb_av1_inv_txfm_add = eb_av1_inv_txfm_add_ssse3;
         if (flags & HAS_AVX2) eb_av1_inv_txfm_add = eb_av1_inv_txfm_add_avx2;