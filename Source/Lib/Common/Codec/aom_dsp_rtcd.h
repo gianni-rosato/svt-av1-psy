b8969f4fe7f7056724c8f4f1c96f7b18f4b4badd
agopikrishna13 <gopi.satykrishna@multicorewareinc.com>
2019-09-11 14:51:26 +0530

    Fix style check failure
    Signed-off-by: agopikrishna13 <gopi.satykrishna@multicorewareinc.com>

--- Source/Lib/Common/Codec/aom_dsp_rtcd.h
+++ Source/Lib/Common/Codec/aom_dsp_rtcd.h
@@ -561,7 +561,6 @@ extern "C" {
     void av1_inv_txfm2d_add_64x64_avx512(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
     RTCD_EXTERN void(*eb_av1_inv_txfm2d_add_64x64)(const int32_t *input, uint16_t *output, int32_t stride, TxType tx_type, int32_t bd);
 
-    
     void eb_av1_highbd_inv_txfm_add_avx2(const int32_t *input, uint16_t *output_r, int32_t stride_r, uint16_t *output_w, int32_t stride_w, TxType tx_type, TxSize tx_size, int32_t eob, int32_t bd);
 
     void eb_av1_inv_txfm2d_add_8x16_c(const int32_t *input, uint16_t *output_r, int32_t stride_r, uint16_t *output_w, int32_t stride_w, TxType tx_type, TxSize tx_size, int32_t eob, int32_t bd);
