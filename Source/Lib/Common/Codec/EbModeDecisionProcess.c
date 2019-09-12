fa5de78159a580f7e04e18db4337e9053bedf8fd
Xu Guangxin <guangxin.xu@intel.com>
2019-09-13 01:13:56 +0800

    EbModeDecisionProcess.c: fix two memory leaks (#604)

--- Source/Lib/Common/Codec/EbModeDecisionProcess.c
+++ Source/Lib/Common/Codec/EbModeDecisionProcess.c
@@ -12,6 +12,12 @@
 static void mode_decision_context_dctor(EbPtr p)
 {
     ModeDecisionContext* obj = (ModeDecisionContext*)p;
+
+#if PRUNE_REF_FRAME_FRO_REC_PARTITION
+    EB_FREE_ARRAY(obj->ref_best_ref_sq_table);
+    EB_FREE_ARRAY(obj->ref_best_cost_sq_table);
+#endif
+
 #if NO_ENCDEC //SB128_TODO to upgrade
     int codedLeafIndex;
     for (codedLeafIndex = 0; codedLeafIndex < BLOCK_MAX_COUNT_SB_128; ++codedLeafIndex) {
