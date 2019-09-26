9e1c88559247e418fe9787639f9d09631cb5e9a3
hguermaz <hassen.guermazi@intel.com>
2019-09-26 00:40:41 -0700

    ATB=f(layer)

--- Source/Lib/Common/Codec/EbPictureDecisionProcess.c
+++ Source/Lib/Common/Codec/EbPictureDecisionProcess.c
@@ -1182,7 +1182,11 @@ EbErrorType signal_derivation_multi_processes_oq(
         // 0                 OFF: no transform partitioning
         // 1                 ON for INTRA blocks
         if (picture_control_set_ptr->enc_mode <= ENC_M1 && sequence_control_set_ptr->static_config.encoder_bit_depth == EB_8BIT)
+#if SPEED_OPT
+            picture_control_set_ptr->atb_mode = (MR_MODE || picture_control_set_ptr->temporal_layer_index == 0) ? 1 : 0;
+#else
             picture_control_set_ptr->atb_mode = 1;
+#endif
         else
             picture_control_set_ptr->atb_mode = 0;
 
