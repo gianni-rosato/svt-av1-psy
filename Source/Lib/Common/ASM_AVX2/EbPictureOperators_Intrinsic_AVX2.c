affa126bc771042b6555772249ffa256fbd00457
wallerjiao <wallerjiao@tencent.com>
2019-11-11 16:00:53 +0800

    Fix issue in unit test: ResidualSumTest.MatchTest/4,13 and 14
    Fix issues:
    1. The data of this ResidualSum avx2 funtion is out of bounds in avx2 register when size = 8 ;
    2. The ResidualSum avx2 function has no implementation code when size = 64;

--- Source/Lib/Common/ASM_AVX2/EbPictureOperators_Intrinsic_AVX2.c
+++ Source/Lib/Common/ASM_AVX2/EbPictureOperators_Intrinsic_AVX2.c
@@ -976,9 +976,11 @@ int32_t  sum_residual8bit_avx2_intrin(
 
         sum = _mm_hadd_epi16(sum, zer);
         sum = _mm_hadd_epi16(sum, zer);
-        sum = _mm_hadd_epi16(sum, zer);
+        // the sum is on 16bit, for negative values, we need to extend the sign to
+        // the next 16bit, so that the next extraction to int32_t is fine.
+        sum = _mm_cvtepi16_epi32(sum);
+        sum = _mm_hadd_epi32(sum, zer);
 
-        sum = _mm_cvtepi16_epi32(sum); //the sum is on 16bit, for negative values, we need to extend the sign to the next 16bit, so that the next extraction to int32_t is fine.
         sumBlock = _mm_cvtsi128_si32(sum);
 
         return sumBlock;
@@ -1071,8 +1073,83 @@ int32_t  sum_residual8bit_avx2_intrin(
         sumBlock = _mm_cvtsi128_si32(sum);
 
         return sumBlock;
-    }
+    } 
 
+    else if (size == 64) {//AVX2 
+        // no more than 11 bit valid residual data is fine, when valid residual data is over 11bit,  the following code need to be rewritten.
+        int16_t* inPtrTemp = in_ptr;
+        __m256i sum4, sum5, sum6, sum7;
+
+        const __m256i ones = _mm256_set1_epi16(1);
+        const __m256i zeros = _mm256_setzero_si256();
+
+        sum0 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp)), _mm256_loadu_si256((__m256i*)(inPtrTemp + 16)));
+        sum1 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + 32)), _mm256_loadu_si256((__m256i*)(inPtrTemp + 48)));
+        sum2 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in)), _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 16)));
+        sum3 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 32)), _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 48)));  
+                                        
+        inPtrTemp += 2 * stride_in;                                 
+                                                                       
+        sum4 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp)), _mm256_loadu_si256((__m256i*)(inPtrTemp + 16)));
+        sum5 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + 32)),  _mm256_loadu_si256((__m256i*)(inPtrTemp  + 48)));
+        sum6 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in)), _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 16)));
+        sum7 = _mm256_add_epi16(_mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 32)), _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 48)));                                        
+
+        inPtrTemp += 2 * stride_in;
+
+        for (row_index = 4; row_index < 64; row_index += 4) {  // Parse every four rows
+            sum0 = _mm256_add_epi16(sum0, _mm256_loadu_si256((__m256i*)(inPtrTemp)));
+            sum1 = _mm256_add_epi16(sum1, _mm256_loadu_si256((__m256i*)(inPtrTemp + 16)));
+            sum2 = _mm256_add_epi16(sum2, _mm256_loadu_si256((__m256i*)(inPtrTemp + 32)));
+            sum3 = _mm256_add_epi16(sum3, _mm256_loadu_si256((__m256i*)(inPtrTemp + 48)));
+
+            sum0 = _mm256_add_epi16(sum0, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in)));
+            sum1 = _mm256_add_epi16(sum1, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 16)));
+            sum2 = _mm256_add_epi16(sum2, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 32)));
+            sum3 = _mm256_add_epi16(sum3, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 48)));
+            
+            inPtrTemp += 2 * stride_in;
+            
+            sum4 = _mm256_add_epi16(sum4, _mm256_loadu_si256((__m256i*)(inPtrTemp)));
+            sum5 = _mm256_add_epi16(sum5, _mm256_loadu_si256((__m256i*)(inPtrTemp + 16)));
+            sum6 = _mm256_add_epi16(sum6, _mm256_loadu_si256((__m256i*)(inPtrTemp + 32)));
+            sum7 = _mm256_add_epi16(sum7, _mm256_loadu_si256((__m256i*)(inPtrTemp + 48)));
+
+            sum4 = _mm256_add_epi16(sum4, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in)));
+            sum5 = _mm256_add_epi16(sum5, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 16)));
+            sum6 = _mm256_add_epi16(sum6, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 32)));
+            sum7 = _mm256_add_epi16(sum7, _mm256_loadu_si256((__m256i*)(inPtrTemp + stride_in + 48)));
+
+            inPtrTemp += 2 * stride_in;
+        }
+
+        sum0 = _mm256_madd_epi16(sum0, ones);
+        sum1 = _mm256_madd_epi16(sum1, ones);
+        sum2 = _mm256_madd_epi16(sum2, ones);
+        sum3 = _mm256_madd_epi16(sum3, ones);
+        
+        sum4 = _mm256_madd_epi16(sum4, ones);
+        sum5 = _mm256_madd_epi16(sum5, ones);
+        sum6 = _mm256_madd_epi16(sum6, ones);
+        sum7 = _mm256_madd_epi16(sum7, ones);
+
+        sum0 = _mm256_add_epi32(sum0, sum1);
+        sum2 = _mm256_add_epi32(sum2, sum3);
+        sum4 = _mm256_add_epi32(sum4, sum5);
+        sum6 = _mm256_add_epi32(sum6, sum7);
+        
+        sum0 = _mm256_add_epi32(sum0, sum2);
+        sum4 = _mm256_add_epi32(sum4, sum6);
+        sum0 = _mm256_hadd_epi32(sum0, zeros);
+        sum4 = _mm256_hadd_epi32(sum4, zeros);
+
+        sum0 = _mm256_add_epi32(sum0, sum4);
+        sum0 = _mm256_hadd_epi32(sum0, zeros);
+
+        sumBlock = _mm_cvtsi128_si32(_mm_add_epi32(_mm256_castsi256_si128(sum0), _mm256_extracti128_si256(sum0, 1)));
+
+        return sumBlock;
+    }
     else {
         printf("\n add the rest \n");
         return 0;
