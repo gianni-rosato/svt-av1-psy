jerrymiao-tencent/SVT-AV1

Add predictive me, bipred3x3, compound mode flags

Source/App/EncApp/EbAppConfig.c
@@ -90,6 +90,9 @@
 #define GLOBAL_MOTION_ENABLE_TOKEN      "-global-motion"
 #define OBMC_TOKEN                      "-obmc"
 #define RDOQ_TOKEN                      "-rdoq"
+#define PRED_ME_TOKEN                   "-pred-me"
+#define BIPRED_3x3_TOKEN                "-bipred-3x3"
+#define COMPOUND_LEVEL_TOKEN            "-compound"
 #define FILTER_INTRA_TOKEN              "-filter-intra"
 #define USE_DEFAULT_ME_HME_TOKEN        "-use-default-me-hme"
 #define HME_ENABLE_TOKEN                "-hme"
@@ -292,6 +295,9 @@ static void SetNsqTableFlag                     (const char *value, EbConfig *cf
 static void SetFrameEndCdfUpdateFlag            (const char *value, EbConfig *cfg) {cfg->frame_end_cdf_update = strtol(value, NULL, 0);};
 static void SetEnableObmcFlag                   (const char *value, EbConfig *cfg) {cfg->enable_obmc = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableRdoqFlag                   (const char *value, EbConfig *cfg) {cfg->enable_rdoq = strtol(value, NULL, 0);};
+static void SetPredictiveMeFlag                 (const char *value, EbConfig *cfg) {cfg->pred_me = strtol(value, NULL, 0); };
+static void SetBipred3x3injectFlag              (const char *value, EbConfig *cfg) {cfg->bipred_3x3_inject = strtol(value, NULL, 0); };
+static void SetCompoundLevelFlag                (const char *value, EbConfig *cfg) {cfg->compound_level = strtol(value, NULL, 0); };
 static void SetEnableFilterIntraFlag            (const char *value, EbConfig *cfg) {cfg->enable_filter_intra = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableHmeFlag                    (const char *value, EbConfig *cfg) {cfg->enable_hme_flag = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableHmeLevel0Flag              (const char *value, EbConfig *cfg) {cfg->enable_hme_level0_flag = (EbBool)strtoul(value, NULL, 0);};
@@ -498,6 +504,14 @@ config_entry_t config_entry[] = {
 
     // Filter Intra
     { SINGLE_INPUT, FILTER_INTRA_TOKEN, "FilterIntra", SetEnableFilterIntraFlag },
+
+    // PREDICTIVE ME
+    { SINGLE_INPUT, PRED_ME_TOKEN, "PredMe", SetPredictiveMeFlag },
+    // BIPRED 3x3 INJECTION
+    { SINGLE_INPUT, BIPRED_3x3_TOKEN, "Bipred3x3", SetBipred3x3injectFlag },
+    // COMPOUND MODE
+    { SINGLE_INPUT, COMPOUND_LEVEL_TOKEN, "CompoundLevel", SetCompoundLevelFlag },
+
     // ME Tools
     { SINGLE_INPUT, USE_DEFAULT_ME_HME_TOKEN, "UseDefaultMeHme", SetCfgUseDefaultMeHme },
     { SINGLE_INPUT, HME_ENABLE_TOKEN, "HME", SetEnableHmeFlag },
@@ -617,6 +631,9 @@ void eb_config_ctor(EbConfig *config_ptr)
     config_ptr->frame_end_cdf_update                 = AUTO_MODE;
     config_ptr->enable_obmc                          = EB_TRUE;
     config_ptr->enable_rdoq                          = AUTO_MODE;
+    config_ptr->pred_me                              = AUTO_MODE;
+    config_ptr->bipred_3x3_inject                    = AUTO_MODE;
+    config_ptr->compound_level                       = AUTO_MODE;
     config_ptr->enable_filter_intra                  = EB_TRUE;
     config_ptr->in_loop_me_flag                      = EB_TRUE;
     config_ptr->use_default_me_hme                   = EB_TRUE;
@@ -1112,6 +1129,22 @@ static EbErrorType VerifySettings(EbConfig *config, uint32_t channelNumber)
     // OBMC
     if (config->enable_obmc != 0 && config->enable_obmc != 1) {
         fprintf(config->error_log_file, "Error instance %u: Invalid OBMC flag [0 - 1], your input: %d\n", channelNumber + 1, config->target_socket);
+        return_error = EB_ErrorBadParameter;
+    }
+
+
+    if (config->pred_me > 5 || config->pred_me < -1) {
+        fprintf(config->error_log_file, "Error instance %u: Invalid predictive me level [0-5, -1 for auto], your input: %d\n", channelNumber + 1, config->pred_me);
+        return_error = EB_ErrorBadParameter;
+    }
+
+    if (config->bipred_3x3_inject > 2 || config->bipred_3x3_inject < -1) {
+        fprintf(config->error_log_file, "Error instance %u: Invalid bipred_3x3_inject mode [0-2, -1 for auto], your input: %d\n", channelNumber + 1, config->bipred_3x3_inject);
+        return_error = EB_ErrorBadParameter;
+    }
+
+    if (config->compound_level > 2 || config->compound_level < -1) {
+        fprintf(config->error_log_file, "Error instance %u: Invalid compound level [0-2, -1 for auto], your input: %d\n", channelNumber + 1, config->compound_level);
         return_error = EB_ErrorBadParameter;
     }
 

Source/Lib/Encoder/Codec/EbEncHandle.c
@@ -2166,6 +2166,13 @@ void CopyApiFromApp(
     // RDOQ
     sequence_control_set_ptr->static_config.enable_rdoq = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->enable_rdoq;
 
+    // Predictive ME
+    sequence_control_set_ptr->static_config.pred_me  = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->pred_me;
+    // BiPred 3x3 injection
+    sequence_control_set_ptr->static_config.bipred_3x3_inject = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->bipred_3x3_inject;
+    // Compound mode
+    sequence_control_set_ptr->static_config.compound_level = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->compound_level;
+
     // Filter intra prediction
     sequence_control_set_ptr->static_config.enable_filter_intra = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->enable_filter_intra;
 
@@ -2740,6 +2747,9 @@ EbErrorType eb_svt_enc_init_parameter(
     config_ptr->frame_end_cdf_update = AUTO_MODE;
     config_ptr->enable_obmc = EB_TRUE;
     config_ptr->enable_rdoq = AUTO_MODE;
+    config_ptr->pred_me = AUTO_MODE;
+    config_ptr->bipred_3x3_inject = AUTO_MODE;
+    config_ptr->compound_level = AUTO_MODE;
     config_ptr->enable_filter_intra = EB_TRUE;
     config_ptr->in_loop_me_flag = EB_TRUE;
     config_ptr->ext_block_flag = EB_FALSE;

gianni-rosato/svt-av1-psy

Source/App/EncApp/EbAppConfig.c
@@ -86,6 +86,9 @@
 #define GLOBAL_MOTION_ENABLE_TOKEN      "-global-motion"
 #define OBMC_TOKEN                      "-obmc"
 #define RDOQ_TOKEN                      "-rdoq"
+#define PRED_ME_TOKEN                   "-pred-me"
+#define BIPRED_3x3_TOKEN                "-bipred-3x3"
+#define COMPOUND_LEVEL_TOKEN            "-compound"
 #define FILTER_INTRA_TOKEN              "-filter-intra"
 #define USE_DEFAULT_ME_HME_TOKEN        "-use-default-me-hme"
 #define HME_ENABLE_TOKEN                "-hme"
@@ -285,6 +288,9 @@ static void SetNsqTableFlag                     (const char *value, EbConfig *cf
 static void SetFrameEndCdfUpdateFlag            (const char *value, EbConfig *cfg) {cfg->frame_end_cdf_update = strtol(value, NULL, 0);};
 static void SetEnableObmcFlag                   (const char *value, EbConfig *cfg) {cfg->enable_obmc = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableRdoqFlag                   (const char *value, EbConfig *cfg) {cfg->enable_rdoq = strtol(value, NULL, 0);};
+static void SetPredictiveMeFlag                 (const char *value, EbConfig *cfg) {cfg->pred_me = strtol(value, NULL, 0); };
+static void SetBipred3x3injectFlag              (const char *value, EbConfig *cfg) {cfg->bipred_3x3_inject = strtol(value, NULL, 0); };
+static void SetCompoundLevelFlag                (const char *value, EbConfig *cfg) {cfg->compound_level = strtol(value, NULL, 0); };
 static void SetEnableFilterIntraFlag            (const char *value, EbConfig *cfg) {cfg->enable_filter_intra = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableHmeFlag                    (const char *value, EbConfig *cfg) {cfg->enable_hme_flag = (EbBool)strtoul(value, NULL, 0);};
 static void SetEnableHmeLevel0Flag              (const char *value, EbConfig *cfg) {cfg->enable_hme_level0_flag = (EbBool)strtoul(value, NULL, 0);};
@@ -516,6 +522,14 @@ config_entry_t config_entry[] = {
 
     // Filter Intra
     { SINGLE_INPUT, FILTER_INTRA_TOKEN, "FilterIntra", SetEnableFilterIntraFlag },
+
+    // PREDICTIVE ME
+    { SINGLE_INPUT, PRED_ME_TOKEN, "PredMe", SetPredictiveMeFlag },
+    // BIPRED 3x3 INJECTION
+    { SINGLE_INPUT, BIPRED_3x3_TOKEN, "Bipred3x3", SetBipred3x3injectFlag },
+    // COMPOUND MODE
+    { SINGLE_INPUT, COMPOUND_LEVEL_TOKEN, "CompoundLevel", SetCompoundLevelFlag },
+
     // ME Tools
     { SINGLE_INPUT, USE_DEFAULT_ME_HME_TOKEN, "UseDefaultMeHme", SetCfgUseDefaultMeHme },
     { SINGLE_INPUT, HME_ENABLE_TOKEN, "HME", SetEnableHmeFlag },
@@ -632,6 +646,9 @@ void eb_config_ctor(EbConfig *config_ptr)
     config_ptr->frame_end_cdf_update                 = DEFAULT;
     config_ptr->enable_obmc                          = EB_TRUE;
     config_ptr->enable_rdoq                          = DEFAULT;
+    config_ptr->pred_me                              = DEFAULT;
+    config_ptr->bipred_3x3_inject                    = DEFAULT;
+    config_ptr->compound_level                       = DEFAULT;
     config_ptr->enable_filter_intra                  = EB_TRUE;
     config_ptr->in_loop_me_flag                      = EB_TRUE;
     config_ptr->use_default_me_hme                   = EB_TRUE;

Source/Lib/Encoder/Codec/EbEncHandle.c
@@ -2167,6 +2167,13 @@ void CopyApiFromApp(
     // RDOQ
     sequence_control_set_ptr->static_config.enable_rdoq = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->enable_rdoq;
 
+    // Predictive ME
+    sequence_control_set_ptr->static_config.pred_me  = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->pred_me;
+    // BiPred 3x3 injection
+    sequence_control_set_ptr->static_config.bipred_3x3_inject = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->bipred_3x3_inject;
+    // Compound mode
+    sequence_control_set_ptr->static_config.compound_level = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->compound_level;
+
     // Filter intra prediction
     sequence_control_set_ptr->static_config.enable_filter_intra = ((EbSvtAv1EncConfiguration*)pComponentParameterStructure)->enable_filter_intra;
 
@@ -2663,6 +2670,21 @@ static EbErrorType VerifySettings(
       return_error = EB_ErrorBadParameter;
     }
 
+    if (config->pred_me > 5 || config->pred_me < -1) {
+      SVT_LOG("Error instance %u: Invalid predictive me level [0-5, -1 for auto], your input: %d\n", channelNumber + 1, config->pred_me);
+      return_error = EB_ErrorBadParameter;
+    }
+
+    if (config->bipred_3x3_inject > 2 || config->bipred_3x3_inject < -1) {
+      SVT_LOG("Error instance %u: Invalid bipred_3x3_inject mode [0-2, -1 for auto], your input: %d\n", channelNumber + 1, config->bipred_3x3_inject);
+      return_error = EB_ErrorBadParameter;
+    }
+
+    if (config->compound_level > 2 || config->compound_level < -1) {
+      SVT_LOG("Error instance %u: Invalid compound level [0-2, -1 for auto], your input: %d\n", channelNumber + 1, config->compound_level);
+      return_error = EB_ErrorBadParameter;
+    }
+
     if (config->combine_class_12 != 0 && config->combine_class_12 != 1 && config->combine_class_12 != -1) {
       SVT_LOG("Error instance %u: Invalid combine MD Class1&2 flag [0/1 or -1 for auto], your input: %d\n", channelNumber + 1, config->combine_class_12);
       return_error = EB_ErrorBadParameter;
@@ -2821,6 +2843,9 @@ EbErrorType eb_svt_enc_init_parameter(
     config_ptr->frame_end_cdf_update = DEFAULT;
     config_ptr->enable_obmc = EB_TRUE;
     config_ptr->enable_rdoq = DEFAULT;
+    config_ptr->pred_me = DEFAULT;
+    config_ptr->bipred_3x3_inject = DEFAULT;
+    config_ptr->compound_level = DEFAULT;
     config_ptr->enable_filter_intra = EB_TRUE;
     config_ptr->in_loop_me_flag = EB_TRUE;
     config_ptr->ext_block_flag = EB_FALSE;