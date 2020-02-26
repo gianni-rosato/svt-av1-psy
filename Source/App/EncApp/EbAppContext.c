Somdyuti2/SVT-AV1

set chroma from luma (cfl) flag cli options

Source/App/EncApp/EbAppContext.c
@@ -143,6 +143,7 @@ EbErrorType copy_configuration_parameters(EbConfig *config, EbAppContext *callba
     callback_data->eb_enc_parameters.bipred_3x3_inject        = config->bipred_3x3_inject;
     callback_data->eb_enc_parameters.compound_level           = config->compound_level;
     callback_data->eb_enc_parameters.set_chroma_mode          = config->set_chroma_mode;
+    callback_data->eb_enc_parameters.cfl_flag                 = config->cfl_flag;
     callback_data->eb_enc_parameters.enable_filter_intra      = (EbBool)config->enable_filter_intra;
     callback_data->eb_enc_parameters.use_default_me_hme       = (EbBool)config->use_default_me_hme;
     callback_data->eb_enc_parameters.enable_hme_flag          = (EbBool)config->enable_hme_flag;

Source/Lib/Encoder/Codec/EbModeDecision.c
@@ -4841,6 +4841,10 @@ void inject_intra_candidates_ois(PictureControlSet *pcs_ptr, ModeDecisionContext
         (MAX(context_ptr->blk_geom->bheight, context_ptr->blk_geom->bwidth) > 32) ? EB_TRUE
                                                                                   : EB_FALSE;
 
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.cfl_flag != DEFAULT)
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.cfl_flag;
+
     OisSbResults *ois_sb_results_ptr = pcs_ptr->parent_pcs_ptr->ois_sb_results[sb_ptr->index];
     OisCandidate *ois_blk_ptr =
         ois_sb_results_ptr
@@ -5315,6 +5319,9 @@ void  inject_intra_candidates(
     uint8_t                     disable_angle_prediction;
     uint8_t directional_mode_skip_mask[INTRA_MODES] = { 0 };
 
+    if (scs_ptr->static_config.cfl_flag != DEFAULT)
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.cfl_flag;
+
     if (context_ptr->edge_based_skip_angle_intra && use_angle_delta)
     {
         EbPictureBufferDesc   *src_pic = pcs_ptr->parent_pcs_ptr->enhanced_picture_ptr;
@@ -5529,6 +5536,10 @@ void  inject_filter_intra_candidates(
 
     EbBool                      disable_cfl_flag = (MAX(context_ptr->blk_geom->bheight, context_ptr->blk_geom->bwidth) > 32) ? EB_TRUE : EB_FALSE;
 
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.cfl_flag != DEFAULT)
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.cfl_flag;
+
     FrameHeader *frm_hdr = &pcs_ptr->parent_pcs_ptr->frm_hdr;
 
     for (filter_intra_mode = intra_mode_start; filter_intra_mode < intra_mode_end ; ++filter_intra_mode) {
@@ -5623,6 +5634,10 @@ void  inject_palette_candidates(
     uint32_t cand_i;
     uint32_t tot_palette_cands = 0;
     PaletteInfo    *palette_cand_array = context_ptr->palette_cand_array;
+
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.cfl_flag != DEFAULT)
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.cfl_flag;
 
     search_palette_luma(
         pcs_ptr,

Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -2106,6 +2106,9 @@ void copy_api_from_app(
     // Chroma mode
     scs_ptr->static_config.set_chroma_mode = ((EbSvtAv1EncConfiguration*)config_struct)->set_chroma_mode;
 
+    // Chroma mode
+    scs_ptr->static_config.cfl_flag = ((EbSvtAv1EncConfiguration*)config_struct)->cfl_flag;
+
     // OBMC
     scs_ptr->static_config.enable_obmc = ((EbSvtAv1EncConfiguration*)config_struct)->enable_obmc;
 
@@ -2668,6 +2671,12 @@ static EbErrorType verify_settings(
       return_error = EB_ErrorBadParameter;
     }
 
+    // Disable chroma from luma (CFL)
+    if (config->cfl_flag != 0 && config->cfl_flag != 1 && config->cfl_flag != -1) {
+        SVT_LOG( "Error instance %u: Invalid CFL flag [0/1, -1], your input: %i\n", channel_number + 1, config->cfl_flag);
+        return_error = EB_ErrorBadParameter;
+    }
+
     // Restoration Filtering
     if (config->enable_restoration_filtering != 0 && config->enable_restoration_filtering != 1 && config->enable_restoration_filtering != -1) {
       SVT_LOG("Error instance %u: Invalid restoration flag [0 - 1, -1 for auto], your input: %d\n", channel_number + 1, config->enable_restoration_filtering);
@@ -2904,6 +2913,7 @@ EbErrorType eb_svt_enc_init_parameter(
     config_ptr->nsq_table = DEFAULT;
     config_ptr->frame_end_cdf_update = DEFAULT;
     config_ptr->set_chroma_mode = DEFAULT;
+    config_ptr->cfl_flag = DEFAULT;
     config_ptr->enable_obmc = EB_TRUE;
     config_ptr->enable_rdoq = DEFAULT;
     config_ptr->pred_me = DEFAULT;

gianni-rosato/svt-av1-psy

Source/App/EncApp/EbAppContext.c
@@ -148,6 +148,7 @@ EbErrorType copy_configuration_parameters(EbConfig *config, EbAppContext *callba
     callback_data->eb_enc_parameters.bipred_3x3_inject        = config->bipred_3x3_inject;
     callback_data->eb_enc_parameters.compound_level           = config->compound_level;
     callback_data->eb_enc_parameters.set_chroma_mode          = config->set_chroma_mode;
+    callback_data->eb_enc_parameters.disable_cfl_flag         = config->disable_cfl_flag;
     callback_data->eb_enc_parameters.enable_filter_intra      = (EbBool)config->enable_filter_intra;
     callback_data->eb_enc_parameters.enable_intra_edge_filter = config->enable_intra_edge_filter;
     callback_data->eb_enc_parameters.use_default_me_hme       = (EbBool)config->use_default_me_hme;

Source/Lib/Encoder/Codec/EbModeDecision.c
@@ -5044,6 +5044,11 @@ void inject_intra_candidates_ois(PictureControlSet *pcs_ptr, ModeDecisionContext
         (MAX(context_ptr->blk_geom->bheight, context_ptr->blk_geom->bwidth) > 32) ? EB_TRUE
                                                                                   : EB_FALSE;
 
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.disable_cfl_flag != DEFAULT && !disable_cfl_flag)
+        // if disable_cfl_flag == 1 then it doesn't matter what cli says otherwise change it to cli
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.disable_cfl_flag;
+
     OisSbResults *ois_sb_results_ptr = pcs_ptr->parent_pcs_ptr->ois_sb_results[sb_ptr->index];
     OisCandidate *ois_blk_ptr =
         ois_sb_results_ptr
@@ -5521,6 +5526,10 @@ void  inject_intra_candidates(
     uint8_t                     disable_angle_prediction;
     uint8_t directional_mode_skip_mask[INTRA_MODES] = { 0 };
 
+    if (scs_ptr->static_config.disable_cfl_flag != DEFAULT && !disable_cfl_flag)
+        // if disable_cfl_flag == 1 then it doesn't matter what cli says otherwise change it to cli
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.disable_cfl_flag;
+
     if (context_ptr->edge_based_skip_angle_intra && use_angle_delta)
     {
         EbPictureBufferDesc   *src_pic = pcs_ptr->parent_pcs_ptr->enhanced_picture_ptr;
@@ -5737,6 +5746,11 @@ void  inject_filter_intra_candidates(
 
     EbBool                      disable_cfl_flag = (MAX(context_ptr->blk_geom->bheight, context_ptr->blk_geom->bwidth) > 32) ? EB_TRUE : EB_FALSE;
 
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.disable_cfl_flag != DEFAULT && !disable_cfl_flag)
+        // if disable_cfl_flag == 1 then it doesn't matter what cli says otherwise change it to cli
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.disable_cfl_flag;
+
     FrameHeader *frm_hdr = &pcs_ptr->parent_pcs_ptr->frm_hdr;
 
     for (filter_intra_mode = intra_mode_start; filter_intra_mode < intra_mode_end ; ++filter_intra_mode) {
@@ -5834,6 +5848,11 @@ void  inject_palette_candidates(
     uint32_t cand_i;
     uint32_t tot_palette_cands = 0;
     PaletteInfo    *palette_cand_array = context_ptr->palette_cand_array;
+
+    SequenceControlSet *scs_ptr = (SequenceControlSet*)pcs_ptr->scs_wrapper_ptr->object_ptr;
+    if (scs_ptr->static_config.disable_cfl_flag != DEFAULT && !disable_cfl_flag)
+        // if disable_cfl_flag == 1 then it doesn't matter what cli says otherwise change it to cli
+        disable_cfl_flag = (EbBool)scs_ptr->static_config.disable_cfl_flag;
 
     search_palette_luma(
         pcs_ptr,

Source/Lib/Encoder/Globals/EbEncHandle.c
@@ -2131,6 +2131,9 @@ void copy_api_from_app(
     // Chroma mode
     scs_ptr->static_config.set_chroma_mode = ((EbSvtAv1EncConfiguration*)config_struct)->set_chroma_mode;
 
+    // Chroma mode
+    scs_ptr->static_config.disable_cfl_flag = ((EbSvtAv1EncConfiguration*)config_struct)->disable_cfl_flag;
+
     // OBMC
     scs_ptr->static_config.enable_obmc = ((EbSvtAv1EncConfiguration*)config_struct)->enable_obmc;
 
@@ -2714,6 +2717,12 @@ static EbErrorType verify_settings(
       return_error = EB_ErrorBadParameter;
     }
 
+    // Disable chroma from luma (CFL)
+    if (config->disable_cfl_flag != 0 && config->disable_cfl_flag != 1 && config->disable_cfl_flag != -1) {
+        SVT_LOG( "Error instance %u: Invalid CFL flag [0/1, -1], your input: %i\n", channel_number + 1, config->disable_cfl_flag);
+        return_error = EB_ErrorBadParameter;
+    }
+
     // CDEF
     if (config->cdef_mode > 5 || config->cdef_mode < -1) {
         SVT_LOG("Error instance %u: Invalid CDEF mode [0 - 5, -1 for auto], your input: %d\n", channel_number + 1, config->cdef_mode);
@@ -2987,6 +2996,7 @@ EbErrorType eb_svt_enc_init_parameter(
     config_ptr->nsq_table = DEFAULT;
     config_ptr->frame_end_cdf_update = DEFAULT;
     config_ptr->set_chroma_mode = DEFAULT;
+    config_ptr->disable_cfl_flag = DEFAULT;
     config_ptr->enable_obmc = EB_TRUE;
     config_ptr->enable_rdoq = DEFAULT;
     config_ptr->pred_me = DEFAULT;