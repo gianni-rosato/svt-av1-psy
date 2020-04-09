2d9167d2588023d32be74238c7e579c6d3f0bf4f
adaria <aopalach@netflix.com>
2020-04-09 12:45:33 -0700

    fix INTRA_ANGLE_DELTA_NEW_TOKEN mapping (#1186)

--- Source/App/EncApp/EbAppConfig.c
+++ Source/App/EncApp/EbAppConfig.c
@@ -1181,8 +1181,8 @@ ConfigEntry config_entry_specific[] = {
     {SINGLE_INPUT, STAT_REPORT_NEW_TOKEN, "Stat Report", set_stat_report},
     {SINGLE_INPUT,
      INTRA_ANGLE_DELTA_NEW_TOKEN,
-     "CDEF Mode, 0: OFF, 1-5: ON with 2,4,8,16,64 step refinement, -1: DEFAULT",
-     set_cdef_mode},
+     "Enable intra angle delta filtering filtering (0: OFF, 1: ON, -1: DEFAULT)",
+     set_intra_angle_delta_flag},
 
     // double dash
     //{SINGLE_INPUT,
