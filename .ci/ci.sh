326f320be02ef0d78f6520fa2835b29624b28955
Cidana Developers <wenyao.liu@cidana.com>
2020-05-26 23:39:10 +0800

    Add unit tests and e2e tests for MacOS CI (#1303)
    1. add unit test and e2e tests in MacOS CI
    2. modified ci.sh to run without root
    
    For self-hosted runner in GitHub CI workflow, please refer to offical guide:
    https://help.github.com/en/actions/hosting-your-own-runners/adding-self-hosted-runners
    
    Co-authored-by: edmond-zhu <edmond.zhu@cidana.com>

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-[ -d "$HOME/.ccache" ] && sudo chown -R $USER: $HOME/.ccache
+if [[ $NOTROOT != 1 ]]; then
+  SUDO=sudo
+fi
+
+[ -d "$HOME/.ccache" ] && $SUDO chown -R $USER: $HOME/.ccache
 export parent_dir=$PWD
 mkdir -p $parent_dir/Build/linux/${build_type:=Release}
 cmake -S $parent_dir -B $parent_dir/Build/linux/$build_type -G"${generator:-Unix Makefiles}" -DCMAKE_BUILD_TYPE=$build_type -DBUILD_SHARED_LIBS=${shared_libs:-ON} -DBUILD_TESTING=${testing:-OFF} ${CMAKE_EFLAGS}
 cmake -j$(nproc) --build $parent_dir/Build/linux/$build_type
-sudo cmake --build $parent_dir/Build/linux/$build_type --target install
+$SUDO cmake --build $parent_dir/Build/linux/$build_type --target install
 $parent_dir/Bin/$build_type/SvtAv1EncApp -help
