1735c9406c9685f00b59f53f284c2b465be126dd
Christopher Degawa <christopher.degawa@intel.com>
2022-12-30 16:02:00 -0600

    stylecheck: make script work with abs path
    Signed-off-by: Christopher Degawa <christopher.degawa@intel.com>

--- test/stylecheck.sh
+++ test/stylecheck.sh
@@ -2,55 +2,58 @@
 
 set -e
 
+REPO_DIR=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")/.." && pwd -P || exit 1)
+
 if ! type git > /dev/null 2>&1; then
     echo "ERROR: git not found, can't continue" >&2
     exit 1
 fi
 
-git fetch --all -pf > /dev/null 2>&1 || true
+git -C "$REPO_DIR" fetch --all -pf > /dev/null 2>&1 || true
 
 echo "Checking for tabs" >&2
-! git --no-pager grep -InP "\t" -- . ':!third_party/**/*' || ret=1
+! git -C "$REPO_DIR" --no-pager grep -InP "\t" -- . ':!third_party/**/*' || ret=1
 
 echo "Checking for carriage returns" >&2
-! git --no-pager grep -InP "\r" -- . ':!third_party/**/*' || ret=1
+! git -C "$REPO_DIR" --no-pager grep -InP "\r" -- . ':!third_party/**/*' || ret=1
 
 echo "Checking for trailing spaces" >&2
-! git --no-pager grep -InP " $" -- . ':!third_party/**/*' ':!*.patch' || ret=1
+! git -C "$REPO_DIR" --no-pager grep -InP " $" -- . ':!third_party/**/*' ':!*.patch' || ret=1
 
 # Test only "new" commits, that is, commits that are not upstream on
 # the default branch.
-if git fetch -q "${CI_MERGE_REQUEST_PROJECT_URL:-https://gitlab.com/AOMediaCodec/SVT-AV1.git}" "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-HEAD}"; then
-    FETCH_HEAD=$(git rev-parse FETCH_HEAD)
+if git -C "$REPO_DIR" fetch -q "${CI_MERGE_REQUEST_PROJECT_URL:-https://gitlab.com/AOMediaCodec/SVT-AV1.git}" "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-HEAD}"; then
+    FETCH_HEAD=$(git -C "$REPO_DIR" rev-parse FETCH_HEAD)
 else
     # in case the fetch failed, maybe internet issues, try to resolve a local default branch's ref, checked-out or not
-    FETCH_HEAD=$(git rev-parse refs/remotes/origin/HEAD)
+    FETCH_HEAD=$(git -C "$REPO_DIR" rev-parse refs/remotes/origin/HEAD)
 fi
 
 # default to master if we have no origin remote
 : "${FETCH_HEAD:=master}"
 
-if ! git merge-tree "$(git merge-base HEAD "$FETCH_HEAD")" HEAD "$FETCH_HEAD" > /dev/null 2>&1; then
+if ! git -C "$REPO_DIR" merge-tree "$(git -C "$REPO_DIR" merge-base HEAD "$FETCH_HEAD")" HEAD "$FETCH_HEAD" > /dev/null 2>&1; then
     echo "ERROR: failed to simulate a merge, check to see if a merge is possible" >&2
 fi
 
-if git diff --exit-code --diff-filter=d --name-only "^$FETCH_HEAD" > /dev/null 2>&1; then
+if git -C "$REPO_DIR" diff --exit-code --diff-filter=d --name-only "^$FETCH_HEAD" > /dev/null 2>&1; then
     echo "No differences to upstream's default, skipping further tests"
     exit 0
 fi
 
-while read -r file; do
+while read -r filename; do
+    file="$REPO_DIR/$filename"
     if ! test -f "$file"; then
-        printf "Ignoring file not found: '%s'\n" "$file"
+        printf "Ignoring file not found: '%s'\n" "$filename"
         continue
     fi
     if test -n "$(tail -c1 "$file")"; then
-        printf "No newline at end of %s\n" "$file"
+        printf "No newline at end of %s\n" "$filename"
         ret=1
     fi
 done << EOF
 $(
-    git diff --name-only --diff-filter=d "$FETCH_HEAD" -- . \
+    git -C "$REPO_DIR" diff --name-only --diff-filter=d "$FETCH_HEAD" -- . \
         ':!third_party' ':!test/e2e_test/test_vector_list.txt' \
         ':!test/vectors/smoking_test.cfg' \
         ':!test/vectors/video_src.cfg' \
@@ -62,7 +65,7 @@ EOF
 
 while read -r i; do
     printf "Checking commit message of %s\n" "$i" >&2
-    msg=$(git log --format=%B -n 1 "$i")
+    msg=$(git -C "$REPO_DIR" log --format=%B -n 1 "$i")
     if test -n "$(printf '%s' "$msg" | sed -n 2p)"; then
         printf "Malformed commit message in %s, second line must be empty\n" "$i"
         ret=1
@@ -75,7 +78,7 @@ while read -r i; do
         printf "Warning: fixup commit detected: %s\n" "$i"
     fi
 done << EOF
-$(git rev-list HEAD "^$FETCH_HEAD")
+$(git -C "$REPO_DIR" rev-list HEAD "^$FETCH_HEAD")
 EOF
 
 if ! type python3 > /dev/null 2>&1; then
@@ -83,17 +86,16 @@ if ! type python3 > /dev/null 2>&1; then
     exit "${ret:-0}"
 fi
 
-MY_DIR=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 1)
 CLANG_FORMAT_URL=https://raw.githubusercontent.com/llvm/llvm-project/main/clang/tools/clang-format/clang-format-diff.py
 
 if type /usr/share/clang/clang-format-diff.py > /dev/null 2>&1; then
     CLANG_FORMAT_DIFF="/usr/share/clang/clang-format-diff.py"
 else
-    if ! type "$MY_DIR/clang-format-diff.py" > /dev/null 2>&1; then
-        curl -ls -o "$MY_DIR/clang-format-diff.py" "$CLANG_FORMAT_URL" ||
-            wget -q -O "$MY_DIR/clang-format-diff.py" "$CLANG_FORMAT_URL"
+    if ! type "$REPO_DIR/test/clang-format-diff.py" > /dev/null 2>&1; then
+        curl -ls -o "$REPO_DIR/test/clang-format-diff.py" "$CLANG_FORMAT_URL" ||
+            wget -q -O "$REPO_DIR/test/clang-format-diff.py" "$CLANG_FORMAT_URL"
     fi
-    CLANG_FORMAT_DIFF="$MY_DIR/clang-format-diff.py"
+    CLANG_FORMAT_DIFF="$REPO_DIR/test/clang-format-diff.py"
 fi
 
 if ! type "$CLANG_FORMAT_DIFF" > /dev/null 2>&1; then
@@ -101,7 +103,7 @@ if ! type "$CLANG_FORMAT_DIFF" > /dev/null 2>&1; then
     exit 1
 fi
 
-diff_output=$(git diff "$FETCH_HEAD" | python3 "$CLANG_FORMAT_DIFF" -p1)
+diff_output=$(cd "$REPO_DIR" && git diff "$FETCH_HEAD" | python3 "$CLANG_FORMAT_DIFF" -p1)
 if [ -n "$diff_output" ]; then
     cat >&2 << 'FOE'
 clang-format check failed!
