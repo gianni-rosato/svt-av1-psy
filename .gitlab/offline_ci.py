7d30e6f03bf78161825ed17c7cafc21f83184e5d
Christopher Degawa <christopher.degawa@intel.com>
2023-01-26 19:00:36 -0600

    offline_ci: instantiate C: if Windows, else /
    Signed-off-by: Christopher Degawa <christopher.degawa@intel.com>

--- .gitlab/offline_ci.py
+++ .gitlab/offline_ci.py
@@ -415,7 +415,7 @@ def clear_build(folder: Path, ret: int = 1) -> int:
 async def docker(project_dir: Path, variables: dict, image: str, extra_args: List[str],
                  log: IOBase) -> Process:
     """Runs docker for a job"""
-    inside_path: Path = Path("/") / REPO_DIR.name
+    inside_path: Path = Path("C:/" if HOST_OS == OS.WINDOWS else "/") / REPO_DIR.name
     args = ["run", "--rm",
             "--cap-add=SYS_PTRACE", "--security-opt", "seccomp=unconfined",
             "--workdir", f"{inside_path}", "-v",
