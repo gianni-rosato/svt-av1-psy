0fd698b4e1ebafa42a524f7d948361826b0882db
Gianni Rosato <giannirosato@proton.me>
2024-02-18 02:57:17 -0500

    feat: CLI prettiness

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -1232,7 +1232,7 @@ ConfigEntry config_entry_psy[] = {
     // Sharpness
     {SINGLE_INPUT,
      SHARPNESS_TOKEN,
-     "Affects loopfilter deblock sharpness and rate distortion, default is 0 [+-7]",
+     "[PSY] Affects loopfilter deblock sharpness and rate distortion, default is 0 [-7 to 7]",
      set_cfg_generic_token},
     // Termination
     {SINGLE_INPUT, NULL, NULL, NULL}};
@@ -1495,7 +1495,7 @@ void svt_config_dtor(EbConfig *app_cfg) {
         fclose(app_cfg->roi_map_file);
         app_cfg->roi_map_file = (FILE *)NULL;
     }
-    
+
 #ifdef LIBDOVI_FOUND
     if (app_cfg->dovi_rpus) {
         dovi_rpu_list_free(app_cfg->dovi_rpus);
@@ -1943,7 +1943,7 @@ int get_version(int argc, char *argv[]) {
 #endif
     if (find_token(argc, argv, VERSION_TOKEN, NULL))
         return 0;
-    printf("SVT-AV1 %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
+    printf("SVT-AV1-PSY %s (%s)\n", svt_av1_get_version(), debug_build ? "release" : "debug");
     return 1;
 }
 
@@ -1953,9 +1953,9 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
         return 0;
 
     printf(
-        "Usage: SvtAv1EncApp <options> <-b dst_filename> -i src_filename\n"
+        "\x1b[1;4mUsage\x1b[0m: SvtAv1EncApp <options> <-b dst_filename> -i src_filename\n"
         "\n"
-        "Examples:\n"
+        "\x1b[1;4mExamples:\n"
         "Multi-pass encode (VBR):\n"
         "    SvtAv1EncApp <--stats svtav1_2pass.log> --passes 2 --rc 1 --tbr 1000 -b dst_filename "
         "-i src_filename\n"
@@ -1965,7 +1965,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
         "Single-pass encode (VBR):\n"
         "    SvtAv1EncApp --passes 1 --rc 1 --tbr 1000 -b dst_filename -i src_filename\n"
         "\n"
-        "Options:\n");
+        "\x1b[1;4mOptions\x1b[0m:\n");
     for (ConfigEntry *options_token_index = config_entry_options; options_token_index->token; ++options_token_index) {
         // this only works if short and long token are one after another
         switch (check_long(*options_token_index, options_token_index[1])) {
@@ -1982,7 +1982,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    options_token_index->name);
         }
     }
-    printf("\nEncoder Global Options:\n");
+    printf("\n\x1b[1;4mEncoder Global Options\x1b[0m:\n");
     for (ConfigEntry *global_options_token_index = config_entry_global_options; global_options_token_index->token;
          ++global_options_token_index) {
         switch (check_long(*global_options_token_index, global_options_token_index[1])) {
@@ -1999,7 +1999,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    global_options_token_index->name);
         }
     }
-    printf("\nRate Control Options:\n");
+    printf("\n\x1b[1;4mRate Control Options\x1b[0m:\n");
     for (ConfigEntry *rc_token_index = config_entry_rc; rc_token_index->token; ++rc_token_index) {
         switch (check_long(*rc_token_index, rc_token_index[1])) {
         case 1:
@@ -2012,7 +2012,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    rc_token_index->name);
         }
     }
-    printf("\nMulti-pass Options:\n");
+    printf("\n\x1b[1;4mMulti-pass Options\x1b[0m:\n");
     for (ConfigEntry *two_p_token_index = config_entry_2p; two_p_token_index->token; ++two_p_token_index) {
         switch (check_long(*two_p_token_index, two_p_token_index[1])) {
         case 1:
@@ -2028,7 +2028,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    two_p_token_index->name);
         }
     }
-    printf("\nGOP size and type Options:\n");
+    printf("\n\x1b[1;4mGOP Size & Type Options\x1b[0m:\n");
     for (ConfigEntry *kf_token_index = config_entry_intra_refresh; kf_token_index->token; ++kf_token_index) {
         switch (check_long(*kf_token_index, kf_token_index[1])) {
         case 1:
@@ -2041,7 +2041,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    kf_token_index->name);
         }
     }
-    printf("\nAV1 Specific Options:\n");
+    printf("\n\x1b[1;4mAV1 Specific Options\x1b[0m:\n");
     for (ConfigEntry *sp_token_index = config_entry_specific; sp_token_index->token; ++sp_token_index) {
         switch (check_long(*sp_token_index, sp_token_index[1])) {
         case 1:
@@ -2054,7 +2054,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
                    sp_token_index->name);
         }
     }
-    printf("\nColor Description Options:\n");
+    printf("\n\x1b[1;4mColor Description Options\x1b[0m:\n");
     for (ConfigEntry *cd_token_index = config_entry_color_description; cd_token_index->token; ++cd_token_index) {
         switch (check_long(*cd_token_index, cd_token_index[1])) {
         case 1:
@@ -2068,7 +2068,7 @@ uint32_t get_help(int32_t argc, char *const argv[]) {
         }
     }
 
-    printf("\nVariance Boost Options:\n");
+    printf("\n\x1b[1;4mPsychovisual Options\x1b[0m:\n");
     for (ConfigEntry *cd_token_index = config_entry_psy; cd_token_index->token; ++cd_token_index) {
         switch (check_long(*cd_token_index, cd_token_index[1])) {
         case 1:
