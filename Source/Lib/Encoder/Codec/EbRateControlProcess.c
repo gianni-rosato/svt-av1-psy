5902ebc19a3dd32827c13d985c0fdd138be7a747
Julio Barba <juliobbv@gmail.com>
2024-02-17 22:46:24 -0500

    Clybius: add `--tune 3` (Subjective SSIM), and `--sharpness`

--- Source/Lib/Encoder/Codec/EbRateControlProcess.c
+++ Source/Lib/Encoder/Codec/EbRateControlProcess.c
@@ -810,7 +810,7 @@ static int crf_qindex_calc(PictureControlSet *pcs, RATE_CONTROL *rc, int qindex)
     // r0 scaling
     // TPL may only look at a subset of available pictures in tpl group, which may affect the r0 calcuation.
     // As a result, we defined a factor to adjust r0 (to compensate for TPL not using all available frames).
-    if (frame_is_intra_only(ppcs)) {
+    if (pcs->scs->static_config.tune == 3 ? svt_aom_frame_is_kf_gf_arf(ppcs) : frame_is_intra_only(ppcs)) {
         if (ppcs->tpl_ctrls.r0_adjust_factor) {
             ppcs->r0 /= ppcs->tpl_ctrls.r0_adjust_factor;
         }
@@ -845,14 +845,14 @@ static int crf_qindex_calc(PictureControlSet *pcs, RATE_CONTROL *rc, int qindex)
         assert(r0_weight_idx <= 2);
         double weight = r0_weight[r0_weight_idx];
         // adjust the weight for base layer frames with shorter minigops
-        if (scs->lad_mg && !frame_is_intra_only(ppcs) &&
+        if (scs->lad_mg && (pcs->scs->static_config.tune == 3 ? !svt_aom_frame_is_kf_gf_arf(ppcs) : !frame_is_intra_only(ppcs)) &&
             (ppcs->tpl_group_size < (uint32_t)(2 << pcs->ppcs->hierarchical_levels)))
             weight = MIN(weight + 0.1, 1);
 
         const double qstep_ratio             = sqrt(ppcs->r0) * weight;
         const int    qindex_from_qstep_ratio = svt_av1_get_q_index_from_qstep_ratio(qindex, qstep_ratio, bit_depth);
 
-        if (!frame_is_intra_only(ppcs))
+        if (pcs->scs->static_config.tune == 3 ? !svt_aom_frame_is_kf_gf_arf(ppcs) : !frame_is_intra_only(ppcs))
             rc->arf_q = qindex_from_qstep_ratio;
         active_best_quality  = clamp(qindex_from_qstep_ratio, rc->best_quality, qindex);
         active_worst_quality = (active_best_quality + (3 * active_worst_quality) + 2) / 4;
@@ -895,6 +895,7 @@ static int crf_qindex_calc(PictureControlSet *pcs, RATE_CONTROL *rc, int qindex)
     assert(ppcs->bottom_index <= rc->worst_quality && ppcs->bottom_index >= rc->best_quality);
     return q;
 }
+#if !TUNE_CQP_CHROMA_SSIM
 /******************************************************
  * non_base_boost
  * Compute a non-base frame boost.
@@ -914,6 +915,7 @@ static int8_t non_base_boost(PictureControlSet *pcs) {
     }
     return q_boost;
 }
+#endif
 
 /******************************************************
  * cqp_qindex_calc
@@ -1172,8 +1174,8 @@ static int compute_deltaq(struct PictureParentControlSet *ppcs, RATE_CONTROL *rc
         deltaq = -ppcs->cyclic_refresh.max_qdelta_perc * q / 100;
     }
     // RA uses a scale factor of 4 for the deltaQ range. Found it beneficial for low delay to have a larger deltaQ range, so we scale by 8
-    deltaq = AOMMIN(deltaq, ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 8 - 1);
-    deltaq = AOMMAX(deltaq, -ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 8 + 1);
+    deltaq = AOMMIN(deltaq, ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (ppcs->scs->static_config.tune == 3 ? 16 : 8) - 1);
+    deltaq = AOMMAX(deltaq, -ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (ppcs->scs->static_config.tune == 3 ? 16 : 8) + 1);
     return deltaq;
 }
 static int av1_estimate_bits_at_q(FrameType frame_type, int q, int mbs, double correction_factor, EbBitDepth bit_depth,
@@ -1334,8 +1336,8 @@ static void generate_b64_me_qindex_map(PictureControlSet *pcs) {
                     : 0;
             }
 
-            offset                      = AOMMIN(offset, pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 4 - 1);
-            offset                      = AOMMAX(offset, -pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 4 + 1);
+            offset                      = AOMMIN(offset, pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (pcs->ppcs->scs->static_config.tune == 3 ? 8 : 4) - 1);
+            offset                      = AOMMAX(offset, -pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (pcs->ppcs->scs->static_config.tune == 3 ? 8 : 4) + 1);
             pcs->b64_me_qindex[b64_idx] = CLIP3(
                 pcs->ppcs->frm_hdr.delta_q_params.delta_q_res,
                 255 - pcs->ppcs->frm_hdr.delta_q_params.delta_q_res,
@@ -1621,8 +1623,8 @@ void svt_aom_sb_qp_derivation_tpl_la(PictureControlSet *pcs) {
                                                    ppcs_ptr->frm_hdr.quantization_params.base_q_idx,
                                                    beta,
                                                    pcs->ppcs->slice_type == I_SLICE);
-            offset         = AOMMIN(offset, pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 4 - 1);
-            offset         = AOMMAX(offset, -pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * 4 + 1);
+            offset         = AOMMIN(offset, pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (pcs->ppcs->scs->static_config.tune == 3 ? 8 : 4) - 1);
+            offset         = AOMMAX(offset, -pcs->ppcs->frm_hdr.delta_q_params.delta_q_res * 9 * (pcs->ppcs->scs->static_config.tune == 3 ? 8 : 4) + 1);
             sb_ptr->qindex = CLIP3(1, // q_index 0 is lossless, and is currently not supported in SVT-AV1
                                    MAXQ,
                                    ((int16_t)ppcs_ptr->frm_hdr.quantization_params.base_q_idx + (int16_t)offset));
@@ -3524,6 +3526,11 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
                 }
             }
 
+            if ((pcs->scs->static_config.tune == 2 || pcs->scs->static_config.tune == 3) && !pcs->ppcs->frm_hdr.delta_q_params.delta_q_present) {
+                // enable sb level qindex when tune 2
+                pcs->ppcs->frm_hdr.delta_q_params.delta_q_present = 1;
+            }
+
             // adjust sb qps based on variance
             if (scs->calculate_variance && scs->static_config.variance_boost_strength) {
                 svt_variance_adjust_qp(pcs,
@@ -3531,10 +3538,6 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
                                        scs->static_config.new_variance_octile);
             }
 
-            if (pcs->scs->static_config.tune == 2 && !pcs->ppcs->frm_hdr.delta_q_params.delta_q_present) {
-                // enable sb level qindex when tune 2
-                pcs->ppcs->frm_hdr.delta_q_params.delta_q_present = 1;
-            }
             if (scs->static_config.rate_control_mode && !is_superres_recode_task) {
                 svt_aom_update_rc_counts(pcs->ppcs);
             }
