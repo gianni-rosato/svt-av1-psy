ed439d20e3ea3d88ed071078cdf628c754f5ce54
Julio Barba <juliobbv@gmail.com>
2024-02-29 00:35:12 -0500

    Add variance boost alternative curve

--- Source/Lib/Codec/pcs.h
+++ Source/Lib/Codec/pcs.h
@@ -1219,6 +1219,7 @@ typedef struct PictureControlSetInitData {
     Bool    enable_variance_boost;
     uint8_t variance_boost_strength;
     uint8_t variance_octile;
+    Bool    enable_alt_curve;
 } PictureControlSetInitData;
 
 typedef struct Av1Comp {
--- Source/Lib/Globals/enc_settings.c
+++ Source/Lib/Globals/enc_settings.c
@@ -876,6 +876,10 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
         return_error = EB_ErrorBadParameter;
     }
 
+    if (config->enable_alt_curve > 1) {
+        SVT_ERROR("Instance %u: Enable alt curve must be between 0 and 1\n", channel_number + 1);
+    }
+
     return return_error;
 }
 
@@ -1025,6 +1029,7 @@ EbErrorType svt_av1_set_default_params(EbSvtAv1EncConfiguration *config_ptr) {
     config_ptr->enable_variance_boost             = FALSE;
     config_ptr->variance_boost_strength           = 2;
     config_ptr->variance_octile                   = 6;
+    config_ptr->enable_alt_curve                  = FALSE;
     return return_error;
 }
 
@@ -1129,10 +1134,11 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
                          config->enable_adaptive_quantization,
                          config->enable_variance_boost);
             } else {
-                SVT_INFO("SVT [config]: AQ mode / variance boost strength / octile \t\t\t: %d / %d / %d\n",
+                SVT_INFO("SVT [config]: AQ mode / variance boost strength / octile / curve \t\t: %d / %d / %d / %s\n",
                          config->enable_adaptive_quantization,
                          config->variance_boost_strength,
-                         config->variance_octile);
+                         config->variance_octile,
+                         config->enable_alt_curve ? "alt" : "regular");
             }
         }
 
@@ -2094,6 +2100,7 @@ EB_API EbErrorType svt_av1_enc_parse_parameter(EbSvtAv1EncConfiguration *config_
         {"enable-dg", &config_struct->enable_dg},
         {"gop-constraint-rc", &config_struct->gop_constraint_rc},
         {"enable-variance-boost", &config_struct->enable_variance_boost},
+        {"enable-alt-curve", &config_struct->enable_alt_curve},
     };
     const size_t bool_opts_size = sizeof(bool_opts) / sizeof(bool_opts[0]);
 
