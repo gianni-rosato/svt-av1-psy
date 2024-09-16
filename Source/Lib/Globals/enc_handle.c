b46f7ed5eb8103d0817c4003c2e0dd654c4882da
stickz <stickman002@mail.com>
2024-09-16 10:34:50 -0400

    Resolve ABR first pass regression
    This merge request resolves a regression with SVT-AV1 v2.2 where the speed of the first pass for ABR - two pass is significant reduced. This happened due to a reduction in the picture analysis processes.
    
    To resolve this problem, the number of picture analysis processes is increased from 4 to 12 for the first pass of ABR - two pass only. The result is an uplift in performance of up to 80% for resolutions 1080p or lower.
    
    Tested on i7-14700k.

--- Source/Lib/Globals/enc_handle.c
+++ Source/Lib/Globals/enc_handle.c
@@ -782,8 +782,9 @@ static EbErrorType load_default_buffer_configuration_settings(
         scs->total_process_init_count += (scs->rest_process_init_count                        = clamp(2, 1, max_rest_proc));
     }
     else if (core_count <= PARALLEL_LEVEL_5_RANGE || scs->input_resolution <= INPUT_SIZE_1080p_RANGE) {
+        const uint8_t pic_processes = scs->static_config.pass == ENC_FIRST_PASS ? 12 : 4;
         scs->total_process_init_count += (scs->source_based_operations_process_init_count     = 1);
-        scs->total_process_init_count += (scs->picture_analysis_process_init_count            = clamp(4, 1, max_pa_proc));
+        scs->total_process_init_count += (scs->picture_analysis_process_init_count            = clamp(pic_processes, 1, max_pa_proc));
         scs->total_process_init_count += (scs->motion_estimation_process_init_count           = clamp(25, 1, max_me_proc));
         scs->total_process_init_count += (scs->tpl_disp_process_init_count                    = clamp(6, 1, max_tpl_proc));
         scs->total_process_init_count += (scs->mode_decision_configuration_process_init_count = clamp(2, 1, max_mdc_proc));
