d49c7e150387934e301fd22829a0dd5efd996f46
Julio Barba <juliobbv@gmail.com>
2024-10-09 18:06:05 -0600

    feat: add custom variance boost curve for tune 4
    Improves CID22, gb82, and subset1 10pct SSIMU2 scores substantially

--- Source/Lib/Codec/rc_process.c
+++ Source/Lib/Codec/rc_process.c
@@ -1459,7 +1459,13 @@ static int av1_get_deltaq_sb_variance_boost(uint8_t base_q_idx, uint16_t *varian
     } else {
         // alternative "flat" q step ratio curve (in log-variance domain)
         // prefers boosting mid-contrast content more over the regular curve (at a modest bitrate increase)
-        qstep_ratio = 0.25 * strength * (-log2((double)variance) + 8) + 1;
+        if (still_picture) {
+            // still picture curve, with variance crossover point at 1024
+            qstep_ratio = 0.15 * strength * (-log2((double)variance) + 10) + 1;
+        } else {
+            // regular curve for video, with variance crossover point at 256
+            qstep_ratio = 0.25 * strength * (-log2((double)variance) + 8) + 1;
+        }
     }
     qstep_ratio = CLIP3(1, VAR_BOOST_MAX_QSTEP_RATIO_BOOST, qstep_ratio);
 
