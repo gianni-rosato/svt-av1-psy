58146ca3aa05751519b19c15593cf0a4a5cd8eff
stickz1 <stickman002@mail.com>
2024-10-25 21:21:35 +0000

    CRF/VBR LP setting optimizations

--- Source/Lib/Globals/enc_handle.c
+++ Source/Lib/Globals/enc_handle.c
@@ -981,7 +981,8 @@ static EbErrorType load_default_buffer_configuration_settings(
         scs->picture_control_set_pool_init_count_child = scs->enc_dec_pool_init_count = clamp(16, min_child, max_child) + superres_count;
     }
     else {
-        scs->picture_control_set_pool_init_count_child = scs->enc_dec_pool_init_count = clamp(18, min_child, max_child) + superres_count;
+        const uint8_t pcs_processes = scs->static_config.rate_control_mode == SVT_AV1_RC_MODE_VBR && scs->static_config.pass == ENC_SECOND_PASS ? 24 : 20;
+        scs->picture_control_set_pool_init_count_child = scs->enc_dec_pool_init_count = clamp(pcs_processes, min_child, max_child) + superres_count;
     }
 #else
     if (core_count == SINGLE_CORE_COUNT || MIN_PIC_PARALLELIZATION) {
@@ -1084,7 +1085,10 @@ static EbErrorType load_default_buffer_configuration_settings(
         scs->total_process_init_count += (scs->rest_process_init_count = clamp(2, 1, max_rest_proc));
     }
     else if (lp <= PARALLEL_LEVEL_5 || scs->input_resolution <= INPUT_SIZE_1080p_RANGE) {
-        const uint8_t pa_processes = scs->static_config.pass == ENC_FIRST_PASS ? 12 : 4;
+        uint8_t pa_processes = 4;
+        if (scs->static_config.pass == ENC_FIRST_PASS) {
+            pa_processes = lp <= PARALLEL_LEVEL_5 ? 12 : 20;
+        }
         scs->total_process_init_count += (scs->source_based_operations_process_init_count = 1);
         scs->total_process_init_count += (scs->picture_analysis_process_init_count = clamp(pa_processes, 1, max_pa_proc));
         scs->total_process_init_count += (scs->motion_estimation_process_init_count = clamp(25, 1, max_me_proc));
@@ -1097,8 +1101,9 @@ static EbErrorType load_default_buffer_configuration_settings(
         scs->total_process_init_count += (scs->rest_process_init_count = clamp(4, 1, max_rest_proc));
     }
     else {
+        const uint8_t pa_processes = scs->static_config.pass == ENC_FIRST_PASS ? 20 : 16;
         scs->total_process_init_count += (scs->source_based_operations_process_init_count = 1);
-        scs->total_process_init_count += (scs->picture_analysis_process_init_count = clamp(16, 1, max_pa_proc));
+        scs->total_process_init_count += (scs->picture_analysis_process_init_count = clamp(pa_processes, 1, max_pa_proc));
         scs->total_process_init_count += (scs->motion_estimation_process_init_count = clamp(25, 1, max_me_proc));
         scs->total_process_init_count += (scs->tpl_disp_process_init_count = clamp(12, 1, max_tpl_proc));
         scs->total_process_init_count += (scs->mode_decision_configuration_process_init_count = clamp(8, 1, max_mdc_proc));
