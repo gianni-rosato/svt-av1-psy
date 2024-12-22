gianni-rosato/svt-av1-psy

commit 389681329d951d6f64a48259f9798c06899b346f

Implement extended CRF support

Source/Lib/Globals/enc_settings.c
@@ -226,6 +226,13 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
         return_error = EB_ErrorBadParameter;
     }
 
+    if ((config->enable_adaptive_quantization || config->variance_boost_strength) && config->extended_crf_qindex_offset > (7 * 4)) {
+        SVT_ERROR("Instance %u: %s must be [0 - %d]\n",
+                  channel_number + 1,
+                  "CRF",
+                  70);
+        return_error = EB_ErrorBadParameter;
+    }
     if (config->qp > MAX_QP_VALUE) {
         SVT_ERROR("Instance %u: %s must be [0 - %d]\n",
                   channel_number + 1,
@@ -233,6 +240,7 @@ EbErrorType svt_av1_verify_settings(SequenceControlSet *scs) {
                   MAX_QP_VALUE);
         return_error = EB_ErrorBadParameter;
     }
+
     if (config->hierarchical_levels > 5) {
         SVT_ERROR("Instance %u: Hierarchical Levels supported [0-5]\n", channel_number + 1);
         return_error = EB_ErrorBadParameter;
@@ -1038,7 +1046,8 @@ EbErrorType svt_av1_set_default_params(EbSvtAv1EncConfiguration *config_ptr) {
     config_ptr->variance_boost_strength           = 2;
     config_ptr->variance_octile                   = 6;
     config_ptr->enable_alt_curve                  = FALSE;
-    config_ptr->sharpness                         = 0;
+    config_ptr->sharpness                         = 0; 
+    config_ptr->extended_crf_qindex_offset        = 0;
     return return_error;
 }
 
@@ -1055,6 +1064,10 @@ static const char *level_to_str(unsigned in) {
     static char ret[313];
     snprintf(ret, 313, "%.1f", in / 10.0);
     return ret;
+}
+
+static double get_extended_crf(EbSvtAv1EncConfiguration *config_ptr) {
+    return (double)config_ptr->qp + (double)config_ptr->extended_crf_qindex_offset / 4;
 }
 
 //#define DEBUG_BUFFERS
@@ -1112,16 +1125,16 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
             if (config->max_bit_rate)
                 SVT_INFO(
                     "SVT [config]: BRC mode / %s / max bitrate (kbps)\t\t\t: %s / %d / "
-                    "%d\n",
+                    "%.2f\n",
                     scs->tpl || scs->static_config.enable_variance_boost ? "rate factor" : "CQP Assignment",
                     scs->tpl || scs->static_config.enable_variance_boost ? "capped CRF" : "CQP",
-                    scs->static_config.qp,
+                    get_extended_crf(config),
                     (int)config->max_bit_rate / 1000);
             else
-                SVT_INFO("SVT [config]: BRC mode / %s \t\t\t\t\t: %s / %d \n",
+                SVT_INFO("SVT [config]: BRC mode / %s \t\t\t\t\t: %s / %.2f \n",
                          scs->tpl || scs->static_config.enable_variance_boost ? "rate factor" : "CQP Assignment",
                          scs->tpl || scs->static_config.enable_variance_boost ? "CRF" : "CQP",
-                         scs->static_config.qp);
+                         get_extended_crf(config));
             break;
         case SVT_AV1_RC_MODE_VBR:
             SVT_INFO("SVT [config]: BRC mode / target bitrate (kbps)\t\t\t\t: VBR / %d \n",
@@ -1282,6 +1295,21 @@ static EbErrorType str_to_uint(const char *nptr, uint32_t *out, char **nextptr) 
     return EB_ErrorNone;
 }
 
+static EbErrorType str_to_double(const char *nptr, double *out, char **nextptr) {
+    char    *endptr;
+    double  val;
+
+    val = strtod(nptr, &endptr);
+
+    if (endptr == nptr || (!nextptr && *endptr))
+        return EB_ErrorBadParameter;
+
+    *out = val;
+    if (nextptr)
+        *nextptr = endptr;
+    return EB_ErrorNone;
+}
+
 //assume the input list of values are in the format of "[v1,v2,v3,...]"
 static EbErrorType parse_list_s32(const char *nptr, int32_t *list, size_t n) {
     const char *ptr = nptr;
@@ -1408,16 +1436,24 @@ static EbErrorType str_to_bool(const char *nptr, Bool *out) {
 }
 
 static EbErrorType str_to_crf(const char *nptr, EbSvtAv1EncConfiguration *config_struct) {
-    uint32_t    crf;
+    double      crf;
     EbErrorType return_error;
 
-    return_error = str_to_uint(nptr, &crf, NULL);
+    return_error = str_to_double(nptr, &crf, NULL);
+
     if (return_error == EB_ErrorBadParameter)
         return return_error;
+    if (crf < 0)
+        return EB_ErrorBadParameter;
 
-    config_struct->qp                           = crf;
+    uint32_t extended_q_index = (uint32_t)(crf * 4);
+    uint32_t qp = AOMMIN(MAX_QP_VALUE, (uint32_t)crf);
+    uint32_t extended_crf_qindex_offset = extended_q_index - qp * 4;
+
+    config_struct->qp                           = qp;
     config_struct->rate_control_mode            = SVT_AV1_RC_MODE_CQP_OR_CRF;
     config_struct->enable_adaptive_quantization = 2;
+    config_struct->extended_crf_qindex_offset   = extended_crf_qindex_offset;
 
     return EB_ErrorNone;
 }

Source/API/EbSvtAv1Enc.h
@@ -997,12 +997,19 @@ typedef struct EbSvtAv1EncConfiguration {
      */
      int8_t sharpness;
 
+    /* @brief Q index for extended CRF support
+     * Value is internally determined by CRF parameter value
+     * Default is 0 if CRF is an integer
+     */
+    uint8_t extended_crf_qindex_offset;
+
     /*Add 128 Byte Padding to Struct to avoid changing the size of the public configuration struct*/
 #if CLN_LP_LVLS
     uint8_t padding[128 - sizeof(Bool) - 2 * sizeof(uint8_t) - sizeof(uint32_t)];
 #else
-    uint8_t padding[128 - 2 * sizeof(Bool) - 2 * sizeof(uint8_t) - sizeof(int8_t)];
+    uint8_t padding[128 - 3 * sizeof(uint8_t) - sizeof(int8_t)];
 #endif
+
 } EbSvtAv1EncConfiguration;
 
 /**

Source/Lib/Codec/rc_process.c
@@ -3222,11 +3222,16 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
                 }
 
                 if (scs->static_config.rate_control_mode == SVT_AV1_RC_MODE_CQP_OR_CRF) {
+                    const int32_t scs_qindex = CLIP3(MIN_Q_INDEX,
+                                                     MAX_Q_INDEX,
+                                                     quantizer_to_qindex[(uint8_t)scs->static_config.qp] + scs->static_config.extended_crf_qindex_offset);
+
                     // if RC mode is 0,  fixed QP is used
                     // QP scaling based on POC number for Flat IPPP structure
                     // make sure no run to run is cause
-                    if (pcs->ppcs->seq_param_changed)
-                        rc->active_worst_quality = quantizer_to_qindex[(uint8_t)scs->static_config.qp];
+                    if (pcs->ppcs->seq_param_changed) {
+                        rc->active_worst_quality = scs_qindex;
+                    }
                     frm_hdr->quantization_params.base_q_idx = quantizer_to_qindex[pcs->picture_qp];
                     if (pcs->ppcs->qp_on_the_fly == TRUE) {
                         pcs->picture_qp = (uint8_t)CLIP3((int32_t)scs->static_config.min_qp_allowed,
@@ -3236,27 +3241,26 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
 
                     } else {
                         if (scs->enable_qp_scaling_flag) {
-                            const int32_t qindex = quantizer_to_qindex[(uint8_t)scs->static_config.qp];
                             // if there are need enough pictures in the LAD/SlidingWindow, the adaptive QP scaling is not used
                             int32_t new_qindex;
                             // if CRF
                             if (pcs->ppcs->tpl_ctrls.enable) {
                                 if (pcs->picture_number == 0) {
-                                    rc->active_worst_quality = quantizer_to_qindex[(uint8_t)scs->static_config.qp];
+                                    rc->active_worst_quality = scs_qindex;
                                     av1_rc_init(scs);
                                 }
                                 new_qindex = crf_qindex_calc(pcs, rc, rc->active_worst_quality);
                             } else // if CQP
-                                new_qindex = cqp_qindex_calc(pcs, qindex);
+                                new_qindex = cqp_qindex_calc(pcs, scs_qindex);
                             frm_hdr->quantization_params.base_q_idx = (uint8_t)CLIP3(
                                 (int32_t)quantizer_to_qindex[scs->static_config.min_qp_allowed],
                                 (int32_t)quantizer_to_qindex[scs->static_config.max_qp_allowed],
                                 (int32_t)(new_qindex));
                         }
 
-                        if (scs->static_config.use_fixed_qindex_offsets || scs->static_config.tune == 3) {
+                        if (scs->static_config.use_fixed_qindex_offsets || scs->static_config.tune == 3 || scs->static_config.extended_crf_qindex_offset) {
                             int32_t qindex = scs->static_config.use_fixed_qindex_offsets == 1
-                                ? quantizer_to_qindex[(uint8_t)scs->static_config.qp]
+                                ? scs_qindex
                                 : frm_hdr->quantization_params
                                       .base_q_idx; // do not shut the auto QPS if use_fixed_qindex_offsets 2
 
@@ -3268,6 +3272,11 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
                                 qindex += scs->static_config.qindex_offsets[pcs->temporal_layer_index];
                             } else {
                                 qindex += scs->static_config.key_frame_qindex_offset;
+                            }
+
+                            // Extended CRF range (63.25 - 70), add offset to all temporal layers to truncate QP scaling
+                            if (scs->static_config.qp == MAX_QP_VALUE && scs->static_config.extended_crf_qindex_offset) {
+                                qindex += scs->static_config.extended_crf_qindex_offset;
                             }
 
                             qindex = CLIP3(quantizer_to_qindex[scs->static_config.min_qp_allowed],
@@ -3292,6 +3301,7 @@ void *svt_aom_rate_control_kernel(void *input_ptr) {
                         chroma_qindex += -rint(chroma_qindex / 8.0); // Chroma boost to fix saturation issues
                     }
 
+                    chroma_qindex += scs->static_config.extended_crf_qindex_offset;
                     chroma_qindex = CLIP3(quantizer_to_qindex[scs->static_config.min_qp_allowed],
                                           quantizer_to_qindex[scs->static_config.max_qp_allowed],
                                           chroma_qindex);

Source/Lib/Globals/enc_handle.c
@@ -4977,6 +4977,9 @@ static void copy_api_from_app(
 
     // Sharpness
     scs->static_config.sharpness = config_struct->sharpness;
+
+    // Extended CRF
+    scs->static_config.extended_crf_qindex_offset = config_struct->extended_crf_qindex_offset;
     return;
 }
 

commit 5d8423e2116c3c28571e9ff93cf87559e6fb3e06

fix: swapped CRF and max bit rate in logs
This commit fixes a regression that was introduced in
389681329d951d6f64a48259f9798c06899b346f, where the wrong format
specifier was updated for extended CRF, leading to CRF and MBR values
being swapped in logs.

Source/Lib/Globals/enc_settings.c
@@ -1174,8 +1174,8 @@ void svt_av1_print_lib_params(SequenceControlSet *scs) {
         case SVT_AV1_RC_MODE_CQP_OR_CRF:
             if (config->max_bit_rate)
                 SVT_INFO(
-                    "SVT [config]: BRC mode / %s / max bitrate (kbps)\t\t\t: %s / %d / "
-                    "%.2f\n",
+                    "SVT [config]: BRC mode / %s / max bitrate (kbps)\t\t\t: %s / %.2f / "
+                    "%d\n",
                     scs->tpl || scs->static_config.enable_variance_boost ? "rate factor" : "CQP Assignment",
                     scs->tpl || scs->static_config.enable_variance_boost ? "capped CRF" : "CQP",
                     get_extended_crf(config),