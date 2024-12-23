e900dc836fea95ddc1703580f66a10d31b22c3a8
BlueSwordM <neutronpcxt@gmail.com>
2024-12-22 19:55:32 -0500

    fix: better RD rshift documentation for tune 3 and slight logic change for loopfilter-sharpness KF boost

--- Source/Lib/Codec/full_loop.c
+++ Source/Lib/Codec/full_loop.c
@@ -1169,6 +1169,8 @@ static void svt_av1_optimize_b(ModeDecisionContext *ctx, int16_t txb_skip_contex
             return;
     }
     int       rweight = 100;
+    // Boost rshift value +2 if a keyframe is present, if tune 3 is active, and if the sharpness below is <=5
+    // Otherwise, assume default behavior for rshift through user selected sharpness value
     const int rshift = (frm_hdr->frame_type == KEY_FRAME && pcs->scs->static_config.tune == 3 && pcs->scs->static_config.sharpness <= 5)
     ? (pcs->scs->static_config.sharpness > 0 ? pcs->scs->static_config.sharpness : 1) + 3
     : (pcs->scs->static_config.sharpness > 0 ? pcs->scs->static_config.sharpness : 1) + 1;
